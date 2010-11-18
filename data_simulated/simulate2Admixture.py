9711f38d0ae3b52f048d9a1162778bcf291ced42
lgo2 <lgo2@208719b8-4595-0410-aefe-b1a4a67cc89b>
2010-11-18 20:16:33 +0000

    Runs simulate2Admixture with different populations and outputs all ancestral individuals not only top ~10.
    git-svn-id: https://forge.cornell.edu/svn/repos/admixture_mapping@25 208719b8-4595-0410-aefe-b1a4a67cc89b

--- data_simulated/simulate2Admixture.py
+++ data_simulated/simulate2Admixture.py
@@ -1,30 +1,33 @@
 import simulateAdmixture as mate, numpy as np
 
 DATA_DIR='../../../human_genome_data/'
-CHR='chr22'
-POP1='yri'
-POP2='mkk'
+CHR='chr1'
+POP1='yoruba'
+POP2='karitiana'
 
-POP_FILES=(DATA_DIR+'data_hapmap3/hapmap3_r2_b36_fwd.consensus.qc.poly.%s_%s.phased.gz'%(CHR,POP1), 
-           DATA_DIR+'data_hapmap3/hapmap3_r2_b36_fwd.consensus.qc.poly.%s_%s.phased.gz'%(CHR, POP2))
-F_GM=DATA_DIR+'genetic_map_%s_b36.txt'%CHR
-NGENS=8
-NOFFSPRING=6
+POP_FILES=(DATA_DIR+'hapmap3/YRI/TRIOS/hapmap3_r2_b36_fwd.consensus.qc.poly.%s_%s.phased.gz'%(CHR,POP1), 
+           DATA_DIR+'hapmap3/MKK/hapmap3_r2_b36_fwd.consensus.qc.poly.%s_%s.phased.gz'%(CHR, POP2))
+POP_FILES=(DATA_DIR+'HumanPhasedData/hgdp/yoruba/hgdp_yoruba.%s.bgl.phased.gz'%CHR,
+           DATA_DIR+'HumanPhasedData/hgdp/karitiana/hgdp_karitiana.%s.bgl.phased.gz'%CHR)
+F_GM=DATA_DIR+'hapmap2/genetic_map_%s_b36.txt'%CHR
+NGENS=20
+NOFFSPRING=16
 
 #Read populations
 pops, nPops, subjects, nSNPs, snpPos, snpNames = mate.readFiles(POP_FILES)
+
 idxPop1=np.random.permutation(nPops[0]) #Pick mating pairs
 idxPop2=np.random.permutation(nPops[1])
 #Mix the first two populations
 admixedPop, admixedOrigin=mate.poissonMating(pops[0][:,idxPop1[:NOFFSPRING]],
                                              pops[1][:,idxPop2[:NOFFSPRING]],
-                                             snpPos, NGENS, .5)
+                                             snpPos, F_GM, NGENS, .5)
 
 # #Save populations
-mate.saveHaplotypes('ancestral_%s.%s.csv'%(POP1, CHR), subjects[0][idxPop1[NOFFSPRING:-10]], snpNames, 
-                    snpPos, pops[0][:,idxPop1[NOFFSPRING:-10]])
-mate.saveHaplotypes('ancestral_%s.%s.csv'%(POP2, CHR), subjects[1][idxPop2[NOFFSPRING:-12]], snpNames, 
-                    snpPos,pops[1][:,idxPop2[NOFFSPRING:-12]])
+mate.saveHaplotypes('ancestral_%s.%s.csv'%(POP1, CHR), subjects[0][idxPop1[NOFFSPRING:]], snpNames, 
+                    snpPos, pops[0][:,idxPop1[NOFFSPRING:]])
+mate.saveHaplotypes('ancestral_%s.%s.csv'%(POP2, CHR), subjects[1][idxPop2[NOFFSPRING:]], snpNames, 
+                    snpPos,pops[1][:,idxPop2[NOFFSPRING:]])
 mate.saveHaplotypes('admixed_%s_%s.%s.csv'%(POP1, POP2, CHR), ['ind']*NOFFSPRING, snpNames, snpPos, admixedPop)
 mate.saveHaplotypes('admixed_origin_%s_%s.%s.csv'%(POP1, POP2, CHR), ['ind']*NOFFSPRING, snpNames, snpPos, admixedOrigin)
 
