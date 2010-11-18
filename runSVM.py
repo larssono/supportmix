f01f208da371739eb75f39a375bb563f48fdf1ae
lgo2 <lgo2@208719b8-4595-0410-aefe-b1a4a67cc89b>
2010-11-18 20:16:09 +0000

    Refactored runSVM completely to make more modular and move computations out of main method.  Added rudimentary hmm code for viterbi algorithm.
    git-svn-id: https://forge.cornell.edu/svn/repos/admixture_mapping@6 208719b8-4595-0410-aefe-b1a4a67cc89b

--- runSVM.py
+++ runSVM.py
@@ -1,111 +1,107 @@
 import PyML, pylab
 import numpy as np
-import gzip
+import fileReader
+import sys
 
-def readSNPs(file):
-    """Reads first and second column of file storing SNP names and position in bp
+WIN_SIZE=100
+SVM=PyML.SVM(C=10, optimizer='mysmo')
+
+def runHMM(snpLocations, admixedClass):
+    """Smoothes out admixedClass using HMM"""
+    import hmm
+    states=[0,1]
+    start_probability = {1: 0.75, 0: 0.25}
+    transition_probability = {0 : {0: 0.7, 1: 0.3},
+                              1 : {0: 0.3, 1: 0.7}}
+    emission_probability = {0 : {0: 0.8, 1: 0.2},
+                            1 : {0: 0.2, 1: 0.8}}
+    results=[]
+    for i in range(admixedClass.shape[1]):
+        p1, states, p2 = hmm.forward_viterbi(admixedClass[:,i],
+                                             states, start_probability, 
+                                             transition_probability,
+                                             emission_probability)
+        results.append(states)
+    return np.array(results).T
+
+
+def createSVMData(vals, labels):
+    """Given array and labels creates PyML with normalization and  Kernel"""
+    dataPyML=PyML.VectorDataSet(vals, L=labels)
+    dataPyML.normalize(1)
+    #dataPyML.attachKernel('polynomial', degree = 1)
+    return dataPyML
+
+def stepSVM(valsTrain, labelsTrain, valsTest, labelsTest):
+    """Does cv on ancestral population then trains on ancestral
+    population followed by testing on admixed population.
     """
-    fp=gzip.open(file); fp.readline()
-    snps = np.asarray([l.split()[0:2] for l in fp], dtype=np.str_)
-    snpNames=snps[:,0]
-    snpLocations=snps[:,1].astype(int)
-    return snpNames, snpLocations
-
+    #test ancestral populations
+    haplotypeData=createSVMData(valsTrain, labelsTrain)
+    results=SVM.cv(haplotypeData, 3);
+    ancestralSuccess = results.getBalancedSuccessRate()
+    #train on ancestral populations
+    SVM.train(haplotypeData);
+    #classify admixed population
+    testData=createSVMData(valsTest, labelsTest)
+    admixedClass = [SVM.classify(testData,i)[0] for i in range(valsTest.shape[0])]
+    return ancestralSuccess, admixedClass
     
 
-def readChrFile(file):
-    """Reads one hapmap3 phased chromosome file
-    Returns subjectList, SNPs
+def trainAndTestSVM(fileNames):
+    """Iterates through files reading WIN_SIZE number of snps then trains and tests
     """
-    fp=gzip.open(file)
-    subjectList=np.asarray(fp.readline().strip().split()[2:], dtype=np.str_)
-    snps=[line.strip().split()[2:] for line in fp]
-    return subjectList, snps, 
+    files=fileReader.concurrentFileReader(fileNames['ceu'], fileNames['yri'], fileNames['asw'])
+    subjects=files.next()
+    nTrain, nTest=len(subjects[0])+len(subjects[1]), len(subjects[2])  #Not Flexible
+    labelsTrain,labelsTest =  ['ceu']*len(subjects[0]) + ['yri']*len(subjects[1]), ['yri']*len(subjects[2])
+    idxTrain, idxTest=range(nTrain), range(nTrain, nTrain+nTest)
+    snpLabels=[]        #stores snp labels from in files
+    snpLocations=[]     #stores physical location from files
+    ancestralSuccess=[] #stores success of ancestral classification
+    admixedClass=[]     #stores classification of test Subjects
+    vals=np.zeros((nTrain+nTest, WIN_SIZE))
+    print vals.shape, nTrain, nTest
 
-def convertSNPs(snps):
-    """Recieves a list of lists of nucleotiedes and converts to numpy array of 0,1
-    """
-    nSubj=len(snps[0])
-    out=np.zeros((len(snps), nSubj), dtype=int)
+    #while True:  #To go through all positions in file
+    for j in range(200):
+        for i, (snpName, snpLocation, snps) in enumerate(files):#range(WIN_SIZE):
+            snpLabels.append(snpName)
+            snpLocations.append(snpLocation)
+            vals[:,i]=fileReader.nucleotides2Haplotypes(sum(snps, []))
+            #vals[:,i]=fileReader.nucleotides2SNPs(sum(snps, []))
+            if i==vals.shape[1]-1:
+                break
+        ancestral, admixed=stepSVM(vals[idxTrain,:i], labelsTrain, 
+                                   vals[idxTest, :i], labelsTest)
+        ancestralSuccess.append(ancestral)
+        admixedClass.append(admixed)
+        if i<WIN_SIZE-1:
+            break
+    admixedClass=np.array(admixedClass)
+    admixedClass=runHMM(snpLocations, admixedClass)
+    return snpLabels, snpLocations, ancestralSuccess, admixedClass
 
-    for i, snp in enumerate(snps):
-        genotypes=list(set(snp))
-        if snp.count(genotypes[0]) >nSubj/2.:
-            majorAllele=genotypes[0]
-        else:
-            majorAllele=genotypes[1]
-    
-        out[i,:]=np.asarray(snp)==majorAllele
-    return out
-    
 
 if __name__ == '__main__':
-    STEPSIZE=200
-    NSTEPS=582
-    asw='hapmap3_r2_b36_fwd.consensus.qc.poly.chr1_asw.phased.gz'
-    ceu='hapmap3_r2_b36_fwd.consensus.qc.poly.chr1_ceu.phased.gz'
-    yri='hapmap3_r2_b36_fwd.consensus.qc.poly.chr1_yri.phased.gz'
-    LABELS=['asw','ceu', 'yri']
-    classes=[]
-    allSubjects=[]
-    allSNPs=[]
-    for i,file in  enumerate([asw, ceu, yri]):
-        subj, snps=readChrFile(file)
-        allSubjects.extend(subj)
-        classes.extend([LABELS[i]]*len(subj))
-        if len(allSNPs)==0:
-            allSNPs=snps
-        else:
-            for i, snp in enumerate(snps):
-                allSNPs[i].extend(snp)
-    data=convertSNPs(allSNPs)
-    data[data==0] = -1
-    snpNames, snpLocations = readSNPs(asw)
-    classes=np.asarray(classes)
-    nSNPs, nSubjects=data.shape
-    
-    idxNASW = classes!='asw'
-    idxASW =  classes=='asw'
-    
-    s=PyML.SVM(C=100)
-    
-    ancestralSuccess=[]
-    admixedYRI=[]
-    for i in range(NSTEPS):
-        #kernel=np.corrcoef(data[i*STEPSIZE:(i+1)*STEPSIZE, idxNASW].T)
-        haplotypeData=PyML.VectorDataSet(data[i*STEPSIZE:(i+1)*STEPSIZE, idxNASW].T, L=list(classes[idxNASW]))
-        haplotypeData.normalize(1)
-        #haplotypeData.attachKernel('polynomial', degree = 1, normalization = 'cosine')
+    fileNames={'ceu': 'data_hapmap3/hapmap3_r2_b36_fwd.consensus.qc.poly.chr22_ceu.phased.gz',
+           'yri': 'data_hapmap3/hapmap3_r2_b36_fwd.consensus.qc.poly.chr22_yri.phased.gz',
+           'asw': 'data_abra_test/LD_0.80.chr22.filtered.txt.gz'}
+           #'asw': 'data_hapmap3/hapmap3_r2_b36_fwd.consensus.qc.poly.chr22_asw.phased.gz'}
+    snpLabels, snpLocations, ancestralSuccess, admixedClass =  trainAndTestSVM(fileNames)
 
-        result=s.cv(haplotypeData, 5)
-        ancestralSuccess.append(result.getBalancedSuccessRate())
-        
-        s.train(haplotypeData)
-        testData=PyML.VectorDataSet(data[i*STEPSIZE:(i+1)*STEPSIZE, idxASW].T, L=['ceu']*sum(idxASW))
-        testData.normalize(1)
-        #testData.attachKernel('polynomial', degree = 1, normalization = 'cosine')
-
-        result = s.test(testData)
-        print result
-        admixedYRI.append(result.getPredictedClass())
-
-    admixedYRI=np.asarray(admixedYRI)
-
+    pylab.clf()
     pylab.subplot(2,1,1);
-    pylab.plot(ancestralSuccess); pylab.xlim([0,NSTEPS])
-    pos, vals=pylab.xticks()    
-    pylab.xticks(pos[1:-1], (snpLocations[(pos[1:-1]*STEPSIZE).astype(int)]/1e6).round())
+    pylab.errorbar(range(len(ancestralSuccess)), map(np.mean, ancestralSuccess), map(np.std, ancestralSuccess))
     pylab.title('Success of SVM on ancestral population')
-    pylab.ylim([0, 1])
+
     pylab.subplot(2,1,2);
-    pylab.imshow(admixedYRI.T, interpolation='nearest')
+    pylab.imshow(admixedClass.T, interpolation='nearest')
     pylab.ylabel('Sample ');pylab.title('Genotype Origin')
     pylab.axis('tight')
-    pos, vals=pylab.xticks()    
-    pylab.xticks(pos[1:-1], (snpLocations[(pos[1:-1]*STEPSIZE).astype(int)]/1e6).round())
-    pylab.xlabel('Mb along chromosome 1')
+    pos, vals=pylab.xticks()
 
     pylab.figure()
-    pylab.hist(admixedYRI.sum(0)/float(NSTEPS), 5)
+    pylab.hist(admixedClass.sum(0), 10) #float(NSTEPS), 10)
     pylab.title('Distribution of Percent African in Population')
-    
+    pylab.show()
