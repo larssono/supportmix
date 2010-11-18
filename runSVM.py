b459c72251c1e266e2adee7963232c3c6fbbe4cd
lgo2 <lgo2@208719b8-4595-0410-aefe-b1a4a67cc89b>
2010-11-18 20:16:26 +0000

    Added command line parameters and seperated code into function to make scripting easier.
    git-svn-id: https://forge.cornell.edu/svn/repos/admixture_mapping@19 208719b8-4595-0410-aefe-b1a4a67cc89b

--- runSVM.py
+++ runSVM.py
@@ -1,41 +1,62 @@
 import fileReader, regionClassifier, sys, gzip,  pylab, numpy as np
+from optparse import OptionParser
 
-WIN_SIZE=150
+MAPFILES='../../human_genome_data/hapmap2/genetic_map_%s_b36.txt'
+USAGE="""%prog [options] ancestralFile1 ancestralFile2 [...] admixedFile
 
-FILES=['data_simulated/yri_mkk/ancestral_yri.chr22.csv',
-       'data_simulated/yri_mkk/ancestral_mkk.chr22.csv',
-       'data_simulated/yri_mkk/admixed_yri_mkk.chr22.csv']
-ADMIXED_CORRECT_FILE='data_simulated/yri_mkk/admixed_origin_yri_mkk.chr22.csv'
-# FILES=['data_simulated/ceu_yri_chd/ancestral_ceu.chr22.csv',
-#        'data_simulated/ceu_yri_chd/ancestral_yri.chr22.csv',
-#        'data_simulated/ceu_yri_chd/ancestral_chd.chr22.csv',
-#        'data_simulated/ceu_yri_chd/admixed_ceu_yri_chd.chr22.csv']
-# ADMIXED_CORRECT_FILE='data_simulated/ceu_yri_chd/admixed_origin_ceu_yri_chd.chr22.csv'
+where the ancestralFiles and admixedFiles are contain phased samples
+in tab delimited format snps in rows and samples in columns.  One row
+of column headers and two columns of header information (i.e. rsId and
+position).  The files have to be from the same chromosome indicated by
+including chr[0-9]* in the name.
+"""
 
-MAPFILE='../../human_genome_data/genetic_map_chr22_b36.txt'
+def determineChromosome(fileNames):
+    """Estimates the chromosome name by searching for "chr" in input
+    file names.
+    Arguments:
+    - `fileNames`:List of fileNames 
+    """
+    import re 
+    p = re.compile('chr\d*', re.IGNORECASE)
+    try:
+        found=[p.search(name).group() for name in fileNames]
+        if not np.all(found[0]==np.asarray(found)): raise Error
+        return found[0] 
+    except:
+        print "ERROR: names of input files should contain the same chr[0-9]*"
+        sys.exit()
 
-def readCorrect():
-    """Read the file of correct classifications and returns
-    the classes or the genotyped class."""
-    fileData=np.array([l.split()[2:] for l in fileReader.openfile(ADMIXED_CORRECT_FILE).readlines()[1:]], np.float)
-    return fileData
+def runSVM(fileNames, nGens=6, svmC=100, win_size=100):
+    """Deconvolves ancestry in last file based on ancestral
+    populations in first files.
+    Arguments:
+    - `fileNames`: list of fileNames
+    - `nGens`: Number of generations since admixture used in hmm filter (default=6)
+    - `svmC`: missclassification penalty term in svm (default=100)
+    - `win_size`: number of snps in each window (defualt=100)
 
-if __name__ == '__main__':
+    Returns:
+    - `snpLocations`: list of positions in bp
+    - `ancestralSuccess`: success of cross validation in ancestral populations
+    - `admixedClassPre`: classification of admixed samples before hmm filter
+    - `admixedClass`:    classification of admixed samples after hmm filter
+    """
+    files=fileReader.concurrentFileReader(*fileNames)
+    chrom=determineChromosome(fileNames)
+
     snpLocations=[]     #stores physical location from files
     ancestralSuccess=[] #stores success of ancestral classification
     admixedClass=[]     #stores classification of test Subjects
+    classifier=regionClassifier.SVMpymvpa(C=svmC)
+    filter=regionClassifier.hmmFilter(geneticMapFile=MAPFILES%chrom,nGens=nGens,nClasses=len(fileNames)-1)
 
-    #classifier=regionClassifier.SVMpyml2(C=100)
-    classifier=regionClassifier.SVMpymvpa(C=100)
-
-    filter=regionClassifier.hmmFilter3(geneticMapFile=MAPFILE,nGens=1)
-    files=fileReader.concurrentFileReader(*FILES)
     subjects=files.next()
     nTrain=np.sum(map(len, subjects[:-1]))  #Number of samples in training set
     nTest=len(subjects[-1]);
-    labelsTrain =sum([[i]*len(sub) for i, sub in enumerate(subjects[:-1])],[])   #Arbitrary labels for ancestral populations
+    labelsTrain =sum([[i]*len(sub) for i, sub in enumerate(subjects[:-1])],[])
 
-    vals=np.zeros((nTrain+nTest, WIN_SIZE))  #temporary storage of output
+    vals=np.zeros((nTrain+nTest, win_size))  #temporary storage of output
     while True: #for j in range(100): #To go through all positions in file
         for i, (snpName, snpLocation, snps) in enumerate(files):
             snpLocations.append(float(snpLocation))
@@ -45,38 +66,58 @@ if __name__ == '__main__':
         ancestral, admixed=classifier(vals[:nTrain,:i], labelsTrain, vals[-nTest:, :i])
         ancestralSuccess.append(ancestral)
         admixedClass.append(admixed)
-        if i<WIN_SIZE-1:
+        if i<win_size-1:
             break
     admixedClassPre=np.array(admixedClass)
     admixedClass=filter(snpLocations, ancestralSuccess, admixedClassPre)
 
-    #Compare and find successRate
-    correct=readCorrect()
-    svmClass=np.repeat(admixedClassPre, WIN_SIZE, 0)
-    hmmClass=np.repeat(admixedClass, WIN_SIZE, 0)
+    return snpLocations, ancestralSuccess, admixedClassPre, admixedClass
 
-    svmSuccess=100-sum(abs(svmClass[:len(correct),:]-correct))/len(correct)*100
-    hmmSuccess=100-sum(abs(hmmClass[:len(correct),:]-correct))/len(correct)*100
-    print np.mean(svmSuccess),'+/-', np.std(svmSuccess), np.mean(hmmSuccess),'+/-',np.std(hmmSuccess)
 
-    pylab.figure(1); pylab.clf()
-    pylab.subplot(4,1,1);
-    pylab.errorbar(range(len(ancestralSuccess)), map(np.mean, ancestralSuccess), map(np.std, ancestralSuccess))
-    pylab.xlim([0, len(ancestralSuccess)]); pylab.xticks([])
-    pylab.ylim([0,1]); pylab.ylabel('Success rate')
-    pylab.title('Ancestral Population')
+if __name__ == '__main__':
+    parser = OptionParser(usage=USAGE)
+    #I want to add stuff describing the arguments
+    parser.add_option("-f", "--fileCorrect", type='string', dest="correctFile", 
+                      help="FILE contains correct classifications in tab delimited format with SNPs in rows and samples in columns (first two columns contain rsID and position. ", metavar="FILE")
+    parser.add_option("-w", "--window", type='int', dest="win", default=10, 
+                      help="Number of SNPs in each window (default 10)", metavar="N")
+    parser.add_option("-g", "--generations", type='int', dest="nGens", default=6,
+                      help="Number of generations sinces admixture used in hmm. (default 6)", metavar="N")
+    (options, args) = parser.parse_args()
 
-    pylab.subplot(4,1,2);
+    fileNames=args
+    snpLocations, ancestralSuccess, admixedClassPre, admixedClass = runSVM(fileNames, nGens=options.nGens, win_size=options.win)
+    for i, pop in enumerate(fileNames[:-1]):
+        pop=pop.split('/')[-1]
+        print 'Pop %s: %2.2g' %(pop, 100*np.sum(admixedClass.flatten()==i)/float(np.prod(admixedClass.shape)))
+
+    chrom=determineChromosome(fileNames)
+    pylab.subplot(2,1,1);
     pylab.imshow(admixedClassPre.T, interpolation='nearest', cmap=pylab.cm.copper, vmin=0, vmax=2)
     pylab.ylabel('Sample '); pylab.yticks([]); pylab.xticks([]); pylab.axis('tight')
-
-    pylab.subplot(4,1,3);
+    pylab.subplot(2,1,2);
     pylab.imshow(admixedClass.T, interpolation='nearest', cmap=pylab.cm.copper, vmin=0, vmax=2)
-    pylab.ylabel('Sample ');pylab.yticks([]); pylab.xticks([]); pylab.axis('tight')
+    pylab.ylabel('Sample ');pylab.yticks([]); pylab.xticks([]); pylab.axis('tight'); 
+    pylab.title('Position along %s' %chrom)
 
-    pylab.subplot(4,1,4);
-    pylab.imshow(correct[:, :].T, interpolation='nearest', cmap=pylab.cm.copper, vmin=0, vmax=2)
-    pylab.ylabel('Sample ');pylab.yticks([]); pylab.xticks([])
-    pylab.xlabel('Position along Chromosome 1');  pylab.axis('tight')
+    if options.correctFile:
+        correct=np.array([l.split()[2:] for l in fileReader.openfile(options.correctFile).readlines()[1:]], np.float)
+        #Compare and find successRate
+        svmClass=np.repeat(admixedClassPre, options.win, 0)
+        hmmClass=np.repeat(admixedClass, options.win, 0)
+        svmSuccess=100-sum(abs(svmClass[:len(correct),:]-correct))/len(correct)*100
+        hmmSuccess=100-sum(abs(hmmClass[:len(correct),:]-correct))/len(correct)*100
+        print 'Correct %0.3g +/- %0.2g (%0.3g +/- %0.2g)' %(np.mean(svmSuccess), np.std(svmSuccess), 
+                                                            np.mean(hmmSuccess), np.std(hmmSuccess))
+        pylab.figure();
+        pylab.subplot(2,1,1);
+        pylab.errorbar(range(len(ancestralSuccess)), map(np.mean, ancestralSuccess), map(np.std, ancestralSuccess))
+        pylab.xlim([0, len(ancestralSuccess)]); pylab.xticks([])
+        pylab.ylim([0,1]); pylab.ylabel('Success rate')
+        pylab.title('Ancestral Population Success Rate')
+        pylab.subplot(2,1,2);
+        pylab.imshow(correct[:, :].T, interpolation='nearest', cmap=pylab.cm.copper, vmin=0, vmax=2)
+        pylab.ylabel('Sample');pylab.yticks([]); pylab.xticks([])
+        pylab.xlabel('Position along %s' %chrom);  pylab.axis('tight'); pylab.title('True ancestry')
+        pylab.show()
     pylab.show()
-    
