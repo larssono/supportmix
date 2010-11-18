4b705a1adfc26cd39b4b60874ed729c0ccce9b09
lgo2 <lgo2@208719b8-4595-0410-aefe-b1a4a67cc89b>
2010-11-18 20:16:13 +0000

    Added support for new hmm model.  Changed plotting.
    git-svn-id: https://forge.cornell.edu/svn/repos/admixture_mapping@9 208719b8-4595-0410-aefe-b1a4a67cc89b

--- runSVM.py
+++ runSVM.py
@@ -2,26 +2,62 @@ import PyML, pylab
 import numpy as np
 import fileReader
 import sys
+from  hmm import hmm
+import gzip
 
 WIN_SIZE=100
-SVM=PyML.SVM(C=10, optimizer='mysmo')
+SVM=PyML.SVM(C=10000, optimizer='mysmo')
+nGens=1
 
-def runHMM(snpLocations, admixedClass):
+class geneticMap(object):
+    """keeps track of genetic Map locations and returns closests genetic map location 
+    given a snp location. """
+
+    def __init__(self,file ):
+        """ """
+        fp=open(file)
+        self.m=np.asarray([np.asarray(l.split())[[0,2]] for l in fp.readlines()[1:]], np.float)
+
+    def pos2gm(self, pos):
+        """Converts position in bp to position in centiMorgans"""
+        m=self.m
+        i=m[:,0].searchsorted(pos)
+        try:
+            if m[i,0]==pos or i==0:
+                return m[i,1]
+            elif i==0:
+                return m[0,1]
+            else:  #linear interpolation
+                return (m[i,1]-m[i-1,1])/(m[i,0]-m[i-1,0])*(pos-m[i-1,0]) + m[i-1,1]
+        except IndexError:
+            if i==len(m):
+                return m[-1,1]
+            else:
+                raise IndexError
+
+def runHMM(mapLocations, successRate, admixedClass):
     """Smoothes out admixedClass using HMM"""
-    import hmm
-    states=[0,1]
-    start_probability = {1: 0.75, 0: 0.25}
-    transition_probability = {0 : {0: 0.7, 1: 0.3},
-                              1 : {0: 0.3, 1: 0.7}}
-    emission_probability = {0 : {0: 0.8, 1: 0.2},
-                            1 : {0: 0.2, 1: 0.8}}
+
+    #determine transition matrices
+    a=[]; b=[]
+    oldPos=0
+    for i in range(0,len(mapLocations), WIN_SIZE):
+        newPos=np.mean(mapLocations[i:i+WIN_SIZE])
+        dM=-(newPos - oldPos)/100*nGens
+        e=np.exp(dM)
+        oldPos=newPos
+        a.append([[e, 1-e],[1-e, e]])
+    a=np.asarray(a)
+    for s in successRate:
+        b.append([[s, 1-s],[1-s, s]])
+    b=np.asarray(b)
+    model=hmm(a, b)
+
     results=[]
     for i in range(admixedClass.shape[1]):
-        p1, states, p2 = hmm.forward_viterbi(admixedClass[:,i],
-                                             states, start_probability, 
-                                             transition_probability,
-                                             emission_probability)
-        results.append(states)
+        model.forward_backward(admixedClass[:,i])
+        maxIdx=model.pstate.argsort(1)[:,-1]
+        results.append(maxIdx)
     return np.array(results).T
 
 
@@ -29,7 +65,7 @@ def createSVMData(vals, labels):
     """Given array and labels creates PyML with normalization and  Kernel"""
     dataPyML=PyML.VectorDataSet(vals, L=labels)
     dataPyML.normalize(1)
-    #dataPyML.attachKernel('polynomial', degree = 1)
+    dataPyML.attachKernel('polynomial', degree = 1)
     return dataPyML
 
 def stepSVM(valsTrain, labelsTrain, valsTest, labelsTest):
@@ -48,10 +84,28 @@ def stepSVM(valsTrain, labelsTrain, valsTest, labelsTest):
     return ancestralSuccess, admixedClass
     
 
-def trainAndTestSVM(fileNames):
-    """Iterates through files reading WIN_SIZE number of snps then trains and tests
-    """
-    files=fileReader.concurrentFileReader(fileNames['ceu'], fileNames['yri'], fileNames['asw'])
+if __name__ == '__main__':
+    ceuFile='data_simulated/ancestral_ceu_chr22.csv.gz'
+    yriFile='data_simulated/ancestral_yri_chr22.csv.gz'
+    aswFile='data_simulated/admixed_asw_chr22_8gens.csv.gz'
+    aswCorrectFile='data_simulated/admixed_asw_chr22_8gens_origin.csv.gz'
+    #mapping reference genome
+    ceuFile='data_hapmap3/hapmap3_r2_b36_fwd.consensus.qc.poly.chr1_ceu.phased.gz'
+    yriFile='data_hapmap3/hapmap3_r2_b36_fwd.consensus.qc.poly.chr1_yri.phased.gz'
+    aswFile='data_hg18/hg18_chr1_hapmapIII.txt.gz'
+    #read Genetic Map
+    gm=geneticMap('data_simulated/genetic_map_chr1_b36.txt')
+    #Cs=[]
+    #Cs_svmSuccessMEAN=[]
+    #Cs_svmSuccessSTD=[]
+    #Cs_hmmSuccessMEAN=[]
+    #Cs_hmmSuccessSTD=[]
+    
+    #for penalty in [1, 10, 100, 1000, 10000, 100000]:
+    #    SVM=PyML.SVM(C=penalty, optimizer='mysmo')
+
+    #Set up storage variables
+    files=fileReader.concurrentFileReader(ceuFile, yriFile, aswFile)
     subjects=files.next()
     nTrain, nTest=len(subjects[0])+len(subjects[1]), len(subjects[2])  #Not Flexible
     labelsTrain,labelsTest =  ['ceu']*len(subjects[0]) + ['yri']*len(subjects[1]), ['yri']*len(subjects[2])
@@ -60,14 +114,14 @@ def trainAndTestSVM(fileNames):
     snpLocations=[]     #stores physical location from files
     ancestralSuccess=[] #stores success of ancestral classification
     admixedClass=[]     #stores classification of test Subjects
-    vals=np.zeros((nTrain+nTest, WIN_SIZE))
-    print vals.shape, nTrain, nTest
+    mapLocations=[]
+    vals=np.zeros((nTrain+nTest, WIN_SIZE))  #temporary storage of output
 
-    #while True:  #To go through all positions in file
-    for j in range(200):
+    while True: #for j in range(100): #To go through all positions in file
         for i, (snpName, snpLocation, snps) in enumerate(files):#range(WIN_SIZE):
             snpLabels.append(snpName)
-            snpLocations.append(snpLocation)
+            snpLocations.append(float(snpLocation))
+            mapLocations.append(gm.pos2gm(snpLocations[-1]))
             vals[:,i]=fileReader.nucleotides2Haplotypes(sum(snps, []))
             #vals[:,i]=fileReader.nucleotides2SNPs(sum(snps, []))
             if i==vals.shape[1]-1:
@@ -78,30 +132,58 @@ def trainAndTestSVM(fileNames):
         admixedClass.append(admixed)
         if i<WIN_SIZE-1:
             break
-    admixedClass=np.array(admixedClass)
-    admixedClass=runHMM(snpLocations, admixedClass)
-    return snpLabels, snpLocations, ancestralSuccess, admixedClass
+    admixedClassPre=np.array(admixedClass)
+    admixedClass=runHMM(mapLocations, ancestralSuccess, admixedClassPre)
 
+    #Compare and find successRate
+    #correct=np.array([l.split()[2:] for l in gzip.open(aswCorrectFile).readlines()[1:]], np.float)
+    svmClass=np.repeat(admixedClassPre, WIN_SIZE, 0)
+    hmmClass=np.repeat(admixedClass, WIN_SIZE, 0)
 
-if __name__ == '__main__':
-    fileNames={'ceu': 'data_hapmap3/hapmap3_r2_b36_fwd.consensus.qc.poly.chr22_ceu.phased.gz',
-           'yri': 'data_hapmap3/hapmap3_r2_b36_fwd.consensus.qc.poly.chr22_yri.phased.gz',
-           'asw': 'data_abra_test/LD_0.80.chr22.filtered.txt.gz'}
-           #'asw': 'data_hapmap3/hapmap3_r2_b36_fwd.consensus.qc.poly.chr22_asw.phased.gz'}
-    snpLabels, snpLocations, ancestralSuccess, admixedClass =  trainAndTestSVM(fileNames)
+    #svmSuccess=100-sum(abs(svmClass[:len(correct),:]-correct))/len(correct)*100
+    #hmmSuccess=100-sum(abs(hmmClass[:len(correct),:]-correct))/len(correct)*100
 
-    pylab.clf()
-    pylab.subplot(2,1,1);
-    pylab.errorbar(range(len(ancestralSuccess)), map(np.mean, ancestralSuccess), map(np.std, ancestralSuccess))
-    pylab.title('Success of SVM on ancestral population')
-
-    pylab.subplot(2,1,2);
-    pylab.imshow(admixedClass.T, interpolation='nearest')
-    pylab.ylabel('Sample ');pylab.title('Genotype Origin')
-    pylab.axis('tight')
-    pos, vals=pylab.xticks()
+    #Visualize and collect output data
+    #Cs.append(penalty)
+    #Cs_svmSuccessMEAN.append(np.mean(svmSuccess))
+    #Cs_svmSuccessSTD.append(np.std(svmSuccess))
+    #Cs_hmmSuccessMEAN.append(np.mean(hmmSuccess))
+    #Cs_hmmSuccessSTD.append(np.std(hmmSuccess))
+    #print np.mean(svmSuccess), np.std(svmSuccess), np.mean(hmmSuccess), np.std(hmmSuccess)
 
     pylab.figure()
-    pylab.hist(admixedClass.sum(0), 10) #float(NSTEPS), 10)
-    pylab.title('Distribution of Percent African in Population')
+    pylab.clf()
+    pylab.subplot(4,1,1);
+    pylab.errorbar(range(len(ancestralSuccess)), map(np.mean, ancestralSuccess), map(np.std, ancestralSuccess))
+    pylab.xlim([0, len(ancestralSuccess)]); pylab.xticks([])
+    pylab.ylim([0,1]); pylab.ylabel('Success rate')
+    pylab.title('Ancestral Population')
+
+
+    pylab.subplot(4,1,2);
+    pylab.imshow(admixedClassPre.T, interpolation='nearest')
+    pylab.ylabel('Sample '); pylab.yticks([]); pylab.xticks([])
+    pylab.axis('tight')
+
+    pylab.subplot(4,1,3);
+    pylab.imshow(admixedClass.T, interpolation='nearest')
+    pylab.ylabel('Sample ');pylab.yticks([]); pylab.xticks([])
+    pylab.axis('tight')
+
+    #pylab.subplot(4,1,4);
+    #pylab.imshow(correct[:, :].T, interpolation='nearest')
+    #pylab.ylabel('Sample ');pylab.yticks([]); pylab.xticks([])
+    #pylab.xlabel('Position along Chromosome 22')
+    #pylab.axis('tight')
+
+#     pylab.figure()
+#     h1,h2,h3= pylab.errorbar(Cs, Cs_svmSuccessMEAN,  Cs_svmSuccessSTD)
+#     h2,h3,h4=pylab.errorbar(Cs, Cs_hmmSuccessMEAN,  Cs_hmmSuccessSTD)
+#     pylab.ylim([90, 110])
+#     pylab.xlabel('Cs')
+#     pylab.ylabel('Success Rate')
+#     pylab.legend((h1, h2),['SVM', 'SVM and HMM'], 4)
+#     ax=pylab.gca()
+#     ax.set_xscale('log')
     pylab.show()
+    
