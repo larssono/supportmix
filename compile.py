c3ab0e5eee36aa716abf8a2218f83ea9d4e44c54
fja32 <fja32@208719b8-4595-0410-aefe-b1a4a67cc89b>
2011-03-29 16:40:35 +0000

    Added os.path for dealing with path operations and finding mapfiles in SupportMix Added the cxFreeze compile script
    git-svn-id: https://forge.cornell.edu/svn/repos/admixture_mapping@53 208719b8-4595-0410-aefe-b1a4a67cc89b

---
+++ compile.py
@@ -0,0 +1,44 @@
+#cxFreeze Compile Script
+#
+#
+#to run type: python compile.py build
+#
+#
+
+import cx_Freeze
+import sys
+import matplotlib
+
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+    includeDependencies = []
+else:
+    #/usr/lib/libblas/libblas.so.3gf.0
+    #("/usr/lib/libblas.so.3gf.0","libblas.so.3gf"),
+    #includeDependencies = [ ("/usr/lib/libblas/libblas.so.3gf.0","libblas.so.3gf"),("/usr/lib/liblapack.so.3gf.0","liblapack.so.3gf"), ("/usr/lib/libgfortran.so.3","libgfortran.so.3"), ("/etc/matplotlibrc","matplotlibrc")] #,( matplotlib.get_data_path(),"mpl-data"),]
+    includeDependencies = [("/usr/share/pyshared/mdp/hinet/hinet.css", "mdp/hinet/hinet.css"),("/usr/share/pyshared/mdp/utils/slideshow.css", "mdp/utils/slideshow.css"),("/usr/lib/libblas/libblas.so.3gf.0","libblas.so.3gf"),
+("/usr/lib/lapack/liblapack.so.3gf.0","liblapack.so.3gf"), ("/usr/lib/libgfortran.so.3","libgfortran.so.3"), ("/etc/matplotlibrc","matplotlibrc"),
+("/usr/lib/libBLT.2.4.so.8.5","libBLT.2.4.so.8.5"), ("/usr/lib/libtk8.5.so.0","libtk8.5.so.0"), ("/usr/lib/libtcl8.5.so.0","libtcl8.5.so.0"), ("/usr/lib64/libz.so","libz.so"), ("/lib64/libc.so.6","libc.so.6"), ("/lib64/ld-linux-x86-64.so.2","ld-linux-x86-64.so.2")] #,( matplotlib.get_data_path(),"mpl-data"),]
+#    includeDependencies=[]
+
+#/usr/lib/pymodules/python2.6/mdp/utils/slideshow.css
+#/usr/share/pyshared/mdp/utils/slideshow.css
+    zipDependencies = [ ( matplotlib.get_data_path(),"mpl-data"),]
+#zipDependencies =[]
+
+
+buildOptions = dict(include_files = includeDependencies,
+        excludes = [""], includes = ["Tkinter","scipy","scipy.misc", "scipy.misc.common"], compressed=True, zip_includes=zipDependencies, copy_dependent_files=True)
+
+executables = [
+        cx_Freeze.Executable("SupportMix", base = base)
+]
+
+cx_Freeze.setup(
+        name = "SupportMix",
+        version = "0.1",
+        description = "Compiled Version of Support Mix",
+        executables = executables,
+        options = dict(build_exe = buildOptions))
---
+++ runCommand.sh
@@ -0,0 +1,1 @@
+./build/exe.linux-x86_64-2.6/SupportMix -w 400 ceu_yri/ancestral_ceu.chr22.csv ceu_yri/ancestral_yri.chr22.csv /home/local/CB/fja32/workspace/SupportMix/src/ceu_yri/admixed_ceu_yri.chr22.csv
