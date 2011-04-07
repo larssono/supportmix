3789ff96dab182c8d981b25a566133b6b62ff675
lgo2 <lgo2@208719b8-4595-0410-aefe-b1a4a67cc89b>
2011-04-07 19:32:33 +0000

    Fixes text output and simplifies runSVM interface.
    git-svn-id: https://forge.cornell.edu/svn/repos/admixture_mapping@58 208719b8-4595-0410-aefe-b1a4a67cc89b

--- runCommand.sh
+++ runCommand.sh
@@ -1,3 +1,3 @@
 #./build/exe.linux-x86_64-2.6/SupportMix -w 400 ceu_yri/ancestral_ceu.chr22.csv ceu_yri/ancestral_yri.chr22.csv /home/local/CB/fja32/workspace/SupportMix/src/ceu_yri/admixed_ceu_yri.chr22.csv
 
-python SupportMix -w 400 ceu_yri/ancestral_ceu.chr22.csv ceu_yri/ancestral_yri.chr22.csv ceu_yri/admixed_ceu_yri.chr22.csv
+SupportMix -w100 -f data_simulated/ceu_yri/admixed_origin_ceu_yri.chr22.csv data_simulated/ceu_yri/ancestral_ceu.chr22.csv data_simulated/ceu_yri/ancestral_yri.chr22.csv data_simulated/ceu_yri/admixed_ceu_yri.chr22.csv
