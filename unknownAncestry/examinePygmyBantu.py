8b0e45ab8d9b998d1644a4283044ee53d8164aeb
lgo2 <lgo2@208719b8-4595-0410-aefe-b1a4a67cc89b>
2011-04-20 18:32:00 +0000

    Missing changes implemented in beginning of March.      *Adds support for doing comparisson to Structure.      *Plots all chromosomes      *saves output to tped files      *Makes it easy to switch between analyzing Pygmy-Bantu and simulated data.      *Fixes code to run with newer version of PyMVPA
    git-svn-id: https://forge.cornell.edu/svn/repos/admixture_mapping@67 208719b8-4595-0410-aefe-b1a4a67cc89b

--- unknownAncestry/examinePygmyBantu.py
+++ unknownAncestry/examinePygmyBantu.py
@@ -1,6 +1,12 @@
-from runSVM import *
+import sys; sys.path.append('..')
+import gzip
+
+import regionClassifier
+import fileReader
 import mvpa.suite as pymvpa
 from scipy.linalg import svd
+import numpy as np
+import pylab
 
 CHROM=1
 DATADIR='/home/lom/current_projects/human_genome_data/HumanPhasedData/pygmy_bantu_tishkoff/'
@@ -89,29 +95,69 @@ def selctAncestralPCA(winVals, idxBantu, idxPygmy,nAncestral=4, pc=-1):
             idx=np.argsort(-vt[pc,idxPygmy])
     return idxPygmy[idx[:nAncestral]], idxPygmy[idx[nAncestral:]]
 
-
+def saveTPedFile(fileName, winSize, chroms, subjects, allSNPNames, allSNPLocations, allAdmixedClass,allP):
+    """Given ancestry assignments for multiple chromosomes save a tped and tfam file
+    Arguments:
+    - `fileName`: Base name of filenames
+    - `chroms`:   list of chromsome numbers
+    - `subjects`: list of list of subjects
+    - `snpNamesLocations`: list of all snp names in each chromsome
+    - `snpLocations`: list of all snp locations in each chromsome
+    - `ddmixedClass`: list of classifications, one item per chromsome
+    - `p`: list of posterior probability for every window
+    """
+    nSubs=134#len(subjects)
+    with open(fileName+'.tfam', 'w') as pedFile:
+        for i, sub in enumerate(subjects[::2]):
+            pedFile.write('%s %s 0 0 0 0\n' %(sub[:-2], sub[:-2]))
+    tPedFp=gzip.open(fileName+".tped.gz","w")
+    tPedProbFp=gzip.open(fileName+".Probs.tped.gz","w")
+    for i, chrom in enumerate(chroms):
+        snpNames=allSNPNames[i]
+        snpLocations=allSNPLocations[i]
+        admixedClass=allAdmixedClass[i]
+        p=allP[i]
+        for j, (rsId, rsPos) in enumerate(zip(snpNames, snpLocations)):
+            tPedFp.write('%s %s %i %i' %(chrom, rsId, j/winSize, rsPos))
+            tPedFp.write(' %i'*nSubs %tuple(admixedClass[j/winSize, :]+1))
+            tPedFp.write('\n')
+            tPedProbFp.write('%s %s %i %i' %(chrom, rsId, j/winSize, rsPos))
+            tPedProbFp.write(' %0.3g'*nSubs %tuple(p[j/winSize, :]))
+            tPedProbFp.write('\n')
+    tPedFp.close()
+    tPedProbFp.close()
 
 
 ###############################################################################################
 ## Main
 ###############################################################################################
-winSize=200; nAncestral=60; nGens=100; winStep=200
-classifier=pymvpa.LinearCSVMC(C=10)
-cvte=pymvpa.CrossValidatedTransferError(pymvpa.TransferError(classifier),
-                                        splitter=pymvpa.OddEvenSplitter(npertarget='equal'))
-AllAncestralSuccess=[]
-AllAdmixedClass=[]
+chroms=range(1,23)
+winSize=50; nAncestral=72; nGens=20; winStep=50
+fileNames=[LEMANDE, NGUMBA, TIKAR_S, BAKOLA, BAKA, BEDZAN]
+alphaFile='pygmy_bantu_alphas.txt'
+# winSize=100; nAncestral=30; nGens=8; winStep=100
+# fileNames=['tmp/ancestral_ceu.chr%i.csv','tmp/admixed_ceu_yri.chr%i.csv']    
+# alphaFile='tmp/admixed_ceu_yri.alpha.chr22.csv'
+# correctFile='tmp/admixed_ceu_yri_origin.chr22.csv'
 
-for CHROM in range(20,23):
-    fileNames=[LEMANDE%CHROM, NGUMBA%CHROM, TIKAR_S%CHROM, BAKOLA%CHROM, BAKA%CHROM, BEDZAN%CHROM]
-    # fileNames=['tmp/pygmybantu_experimentation_20101014/ancestral_yoruba.chr%i.csv' %CHROM,
-    #           'tmp/pygmybantu_experimentation_20101014/admixed_yoruba_karitiana.chr%i.csv.gz' %CHROM]    
-    subjects, labels, snpNames, snpLocations, vals=readFiles(fileNames)
+classifier=pymvpa.LinearCSVMC(C=10)
+cvte=pymvpa.CrossValidation(classifier, pymvpa.NGroupPartitioner(3, attr='runtype'))
+alphas=[1-float(f.strip().split()[-1]) for f in open(alphaFile)]
+allAdmixedClass=[]
+allAdmixedP=[]
+allSNPLocations=[]
+allSNPNames=[]
+
+#Run the ancestry assignemts
+for CHROM in chroms:
+    subjects, labels, snpNames, snpLocations, vals=readFiles([f%CHROM for f in fileNames])
     origLabels=labels.copy()
-    idxBantu=pylab.find((labels==0)+(labels==1)+(labels==2))
-    idxPygmy=pylab.find((labels==3)+(labels==4)+(labels==5))
-    #idxBantu=pylab.find(labels==0)
-    #idxPygmy=pylab.find(labels==1)
+    if len(fileNames)==6:  #Real data
+        idxBantu=pylab.find((labels==0)+(labels==1)+(labels==2))
+        idxPygmy=pylab.find((labels==3)+(labels==4)+(labels==5))
+    else: #Simulated data
+        idxBantu=pylab.find(labels==0)
+        idxPygmy=pylab.find(labels==1)
     labels[idxBantu]=0
     labels[idxPygmy]=1
     nSubs, nSNPs=vals.shape
@@ -120,84 +166,95 @@ for CHROM in range(20,23):
         winVals=vals[:,startPos:startPos+winSize]
         idx=(np.abs(winVals.sum(0))!=nSubs)     #Filter any SNPs without information across samples
         winVals=winVals[:, idx]
-
-        ##Select Ancestral Pygmy based on cross validation
-        #idxAncestralPygmy, idxAdmixedPygmy = selctAncestralCorrelation(winVals, idxBantu, idxPygmy, nAncestral)
-        #idxAncestralPygmy, idxAdmixedPygmy = selctAncestralPCA(winVals, idxBantu, idxPygmy, nAncestral, -1)
-        #idxAncestralPygmy, idxAdmixedPygmy = selectAncestralRan(winVals, idxBantu, idxPygmy, nAncestral)
-        idxAncestralPygmy, idxAdmixedPygmy = selectAncestralL2Norm(winVals, idxBantu, idxPygmy, nAncestral)
-
-        idxAncestral=list(idxBantu)+list(idxAncestralPygmy)
-        ancestralSamples[:,startPos:startPos+winSize]=vals[idxAncestralPygmy,startPos:startPos+winSize]
-        #Cross Validate ancestral Success rate
-        ds=pymvpa.Dataset(winVals[idxAncestral,:], sa={'targets':labels[idxAncestral], 'chunks':range(len(idxAncestral))})
-        cv_result=1-np.mean(cvte(ds))
-        ancestralSuccess.append(cv_result)
-        # print startPos, winVals.shape, cv_result
-
-        ## Train on ancestral then classify admixed Pygmies
-        classifier.train(ds)
-        admixedClassWin=np.ones(len(idxPygmy))  #The ancestral "Pygmy" are classified as 1 i.e. Pygmy the rest are asigned below
-        admixedClassWin[idxAdmixedPygmy-len(idxBantu)]=classifier.predict(winVals[idxAdmixedPygmy,:])
-
-        #Play with the number of admixed on each side of plane
-        # estimates=classifier.ca['estimates'].value
+        if winVals.shape[1]>3:  #Enough information to determine results
+            ##Select Ancestral my based on...
+            idxAncestralPygmy, idxAdmixedPygmy = selectAncestralL2Norm(winVals, idxBantu, idxPygmy, nAncestral)
+            idxAncestral=list(idxBantu)+list(idxAncestralPygmy)
+            ancestralSamples[:,startPos:startPos+winSize]=vals[idxAncestralPygmy,startPos:startPos+winSize]
+            #Cross Validate ancestral Success rate
+            runtype=np.zeros(len(idxAncestral)); runtype[0::3]=0;runtype[1::3]=1; runtype[2::3]=2 
+            ds=pymvpa.Dataset(winVals[idxAncestral,:], sa={'targets':labels[idxAncestral], 'runtype':runtype, 'chunks':range(len(idxAncestral))})
+            cv_result=1-np.mean(cvte(ds))
+            ancestralSuccess.append(cv_result)
+            ## Train on ancestral then classify admixed Pygmies
+            classifier.train(ds)
+            admixedClassWin=np.ones(len(idxPygmy))  #The ancestral "Pygmy" are classified as 1 i.e. Pygmy the rest are asigned below
+            admixedClassWin[idxAdmixedPygmy-len(idxBantu)]=classifier.predict(winVals[idxAdmixedPygmy,:])
+        else:  #Window is too small to determine anything just assign ancestry to 1
+            admixedClassWin=np.ones(len(idxPygmy))
+            ancestralSuccess.append(0.5)
 
         admixedClass.append(admixedClassWin)
         startPos+=winStep
     admixedClassPre=np.asarray(admixedClass)
-    #admixedClass=admixedClassPre
     smoother=regionClassifier.hmmFilter(geneticMapFile=MAPFILES%CHROM,nGens=nGens,nClasses=2)
-    admixedClass, p=smoother(snpLocations, ancestralSuccess, admixedClassPre)  #TODO fix so this work
+    admixedClass, p=smoother(snpLocations, ancestralSuccess, admixedClassPre)  
+    #Store all results
+    allAdmixedClass.append(admixedClass)
+    allAdmixedP.append(p)
+    allSNPLocations.append(snpLocations)
+    allSNPNames.append(snpNames)
+    #Print summary of results
+    print 'Chromosome %i: Percent Pygmy: %0.2g%% (%0.2g) ' %(CHROM,(admixedClass==1).sum()/float(np.prod(admixedClass.shape))*100, float(nAncestral)/len(idxPygmy)*100)
 
-    nPygmy=admixedClass.shape[1]
-    print 'Chromosome %i'%CHROM
-    print 'Percent Pygmy: %0.2g%% (%0.2g)' %((admixedClass==1).sum()/float(np.prod(admixedClass.shape))*100, float(nAncestral)/len(idxPygmy)*100)
-    #print 'Percent Success: %0.2g %%' %(np.mean(ancestralSuccess)*100)
-    print '-'*40
-    # sizeBP, sizeCM=winSizeBPandCM(snpLocations, winSize, CHROM)
-    pylab.figure()
-    pylab.axes([0.1, 0.4, .6, .5])
-    pylab.title('Chromosome %i' %CHROM)
+#Plot results of multiple Chromsomes
+pylab.figure(figsize=(19.5,5))
+nPygmy=admixedClass.shape[1]
+nWins=[x.shape[0] for x in allAdmixedClass]
+xStart=0.03
+for i, CHROM in enumerate(chroms):
+    admixedClass=allAdmixedClass[i]
+    p=allAdmixedP[i]
+    nWin=nWins[i]
+    snpLocations=allSNPLocations[i]
+    xWidth=.84*(nWin/float(np.sum(nWins)))
+    pylab.axes([xStart, 0.29, xWidth, .52])
     pylab.imshow(((admixedClass*2-1)*p).T, interpolation='nearest', cmap=pylab.cm.RdBu)
-    pylab.axis('tight'); xticks=range(int(pylab.xticks()[0][1]), int(pylab.xticks()[0][-1]), 10)# xticks=range(#xticks=np.asarray(pylab.xticks()[0][1:-1], np.int)
-    pylab.xticks([]); pylab.ylabel('Pygmy Samples')
-    #Plot average Pygmy Ancestry per sample
-    pylab.axes([0.71, 0.4, .25, .5])
-    #Ancestry per position
-    pylab.plot(admixedClass.mean(0), range(nPygmy)); pylab.ylim(nPygmy, 0); pylab.yticks([]); pylab.title('Percent Pygmy'); pylab.xlim(0,1); pylab.xticks([0,.5,1])
-    pylab.axes([0.1, 0.1, .6, .28])
+    pylab.axis('tight');     pylab.title('Chr %i' %CHROM, fontsize=8)
+    xtickPos=np.asarray([2, round(nWin/2), nWin-2], np.int)
+    pylab.axis('off'); 
+    if xStart==0.03:
+        pylab.axis('on'); pylab.xticks([]); 
+        pylab.yticks([0, 24,58,83,108,121,134], ['|', 'Bakola', '|','Baka','|','Bedzan', '|'], fontsize=8, rotation=90)
+    pylab.axes([xStart, 0.1, xWidth, .18])  #Average Pygmy Ancestry
     pylab.plot(admixedClass.mean(1))
-    pylab.ylim([0, 1])
-    pylab.ylabel('Percent Pygmy')
-    pylab.xlim(0, p.shape[0])
-    pylab.xticks(xticks, np.asarray(np.round(snpLocations[::winStep][xticks]/1e6), np.int))
-    pylab.xlabel('Position [Mb]')
+    pylab.ylim([0.5, 1]); pylab.xlim(0, p.shape[0]); pylab.yticks([])
+    pylab.xticks(xtickPos, np.asarray(np.round(snpLocations[::winStep][xtickPos]/1e6), np.int), fontsize=6)
+    if xStart==0.03:
+        pylab.yticks([.5,1], fontsize=8); pylab.ylabel('Percent Pygmy', fontsize=8);
+    if xStart>0.45 and xStart<0.5:
+        pylab.xlabel('Position [Mb]', fontsize=8)
     pylab.draw()
-    #Colorbar
-    pylab.axes([0.71, 0.1, .25, .28]); pylab.axis('off')
-    pylab.colorbar(fraction=1, aspect=4)
-    pylab.text(-10,0.85, 'Pygmy')
-    pylab.text(-10,0.0, 'Bantu')
+    xStart+=(xWidth+0.002)
+#Plot average Pygmy Ancestry per sample
+pylab.axes([0.92, 0.29, .07, .52]) 
+pylab.plot(np.vstack(allAdmixedClass).mean(0), range(nPygmy)); pylab.ylim(nPygmy, 0); pylab.yticks([]); pylab.title('Percent Pygmy'); pylab.xlim(0,1); pylab.xticks([0,.5,1], fontsize=8)
+pylab.plot(alphas, range(nPygmy)); pylab.ylim(nPygmy, 0); pylab.yticks([]); pylab.title('Percent Pygmy', fontsize=8); pylab.xlim(0,1); pylab.xticks([0,.5,1])
+#Colorbar
+pylab.axes([0.925, 0.1, .06, .18]);  pylab.xlim(-1, 1); pylab.ylim(-1, 1); pylab.axis('off')
+pylab.colorbar(fraction=.5, aspect=4, orientation='horizontal', ticks=[-.99, 0, .99])
+pylab.text(-1,-1.5, 'Bantu', fontsize=8)
+pylab.text(1,-1.5, 'Pygmy', fontsize=8, horizontalalignment='right')
+
+#Save output in tped plink files
+saveTPedFile('pygmy_admixture_72b', winSize, chroms, np.hstack(subjects[3:]), allSNPNames, allSNPLocations, allAdmixedClass,allAdmixedP)
 
 
-# pylab.figure();plotPCA(np.vstack((vals, ancestralSamples)), np.hstack((labels, [4]*nAncestral)))
-# pylab.show()
+# If simulated data calculate Success Rate
+if locals().get('correctFile'):
+    correct=np.array([l.split()[2:] for l in fileReader.openfile(correctFile).readlines()[1:]], np.float)
+    svmClass=np.repeat(admixedClassPre, winSize, 0)
+    hmmClass=np.repeat(admixedClass, winSize, 0)
+    svmSuccess=100-sum(abs(svmClass[:len(correct),:]-correct))/len(correct)*100
+    hmmSuccess=100-sum(abs(hmmClass[:len(correct),:]-correct))/len(correct)*100
+    print 'Correct %0.3g +/- %0.2g (%0.3g +/- %0.2g)' %(np.mean(hmmSuccess), np.std(hmmSuccess), np.mean(svmSuccess), np.std(svmSuccess))
+    pylab.figure();
+    pylab.subplot(2,1,1);
+    pylab.imshow(((admixedClass*2-1)*p).T, interpolation='nearest', cmap=pylab.cm.copper, vmin=0, vmax=2)
+    pylab.ylabel('Sample '); pylab.yticks([]); pylab.xticks([]); pylab.axis('tight'); pylab.title('Estimat')
+    pylab.subplot(2,1,2);
+    pylab.imshow(correct[:, :].T, interpolation='nearest', cmap=pylab.cm.copper, vmin=0, vmax=2)
+    pylab.ylabel('Sample');pylab.yticks([]); pylab.xticks([])
+    pylab.xlabel('Position along %s' %CHROM);  pylab.axis('tight'); pylab.title('True ancestry')
 
-#####Simulated Results
-# correctFile='tmp/pygmybantu_experimentation_20101014/admixed_origin_yoruba_karitiana.chr%i.csv.gz'%CHROM
-# correct=np.array([l.split()[2:] for l in fileReader.openfile(correctFile).readlines()[1:]], np.float)
-# svmClass=np.repeat(admixedClassPre, winSize, 0)
-# hmmClass=np.repeat(admixedClass, winSize, 0)
-# svmSuccess=100-sum(abs(svmClass[:len(correct),:]-correct))/len(correct)*100
-# hmmSuccess=100-sum(abs(hmmClass[:len(correct),:]-correct))/len(correct)*100
-# print 'Correct %0.3g +/- %0.2g (%0.3g +/- %0.2g)' %(np.mean(hmmSuccess), np.std(hmmSuccess), np.mean(svmSuccess), np.std(svmSuccess))
-# pylab.figure();
-# pylab.subplot(2,1,1);
-# pylab.imshow(((admixedClass*2-1)*p).T, interpolation='nearest', cmap=pylab.cm.copper, vmin=0, vmax=2)
-# pylab.ylabel('Sample '); pylab.yticks([]); pylab.xticks([]); pylab.axis('tight'); pylab.title('Estimat')
-# pylab.subplot(2,1,2);
-# pylab.imshow(correct[:, :].T, interpolation='nearest', cmap=pylab.cm.copper, vmin=0, vmax=2)
-# pylab.ylabel('Sample');pylab.yticks([]); pylab.xticks([])
-# pylab.xlabel('Position along %s' %CHROM);  pylab.axis('tight'); pylab.title('True ancestry')
-# ###########################
+pylab.show()
