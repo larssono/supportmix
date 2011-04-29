1ea5e089c651273405a1ee52d9f57493440224b4
fja32 <fja32@208719b8-4595-0410-aefe-b1a4a67cc89b>
2011-04-29 20:36:05 +0000

    Created GLOBAL parameters in supportConfig module. Added config options for Parallel and MapDir. Moved of command line and config file options to the supportConfig module. Moved config validation functions to supportConfig. Added option to create an "example" default configuration.
    git-svn-id: https://forge.cornell.edu/svn/repos/admixture_mapping@76 208719b8-4595-0410-aefe-b1a4a67cc89b

--- supportConfig.py
+++ supportConfig.py
@@ -1,12 +1,46 @@
 import sys
 import os
 import ConfigParser
-from optparse import OptionParser
+import pylab
+#from optparse import OptionParser
+from optparse import OptionParser, OptionGroup, OptionValueError
+DEBUG=False
+
+
+CHROM=None
+WIN=None
+N_GENS=None
+SAVE_FILE=None
+CORRECT_FILE=None
+FILE_NAMES=[]
+DO_PLOT=None
+RGB=None
+CONFIGFILE=None
+LABELS=None
+IS_BEAGLE=None
+MAP_DIR=None
+N_PARALLEL=None
+
+
+USAGE="""%prog [options] ancestralFile1 ancestralFile2 [...] admixedFile
+
+where the ancestralFiles and admixedFiles are contain phased samples
+in tab delimited format snps in rows and samples in columns.  One row
+of column headers and two columns of header information (i.e. rsId and
+position).  The files have to be from the same chromosome indicated by
+including chr[0-9]* in the name.
+"""
+
 
 def fail(parser, str):
     sys.stderr.write('SupportMix ERROR: %s\n\n' %str)
     parser.print_usage()
     sys.exit(1)
+
+
+def warn(str):
+    sys.stderr.write('SupportMix WARNING!: %s\n' %str)
+    
 
 
 def createDataPath(dataPath='data'):
@@ -25,23 +59,27 @@ def readConfig(configFile="supportMix.cfg"):
     config.read(configFile)
     return config
 
-#def validateConfig(configFile):
-#    config=readConfig()
+##Thinking of moving some of the validation here
+#def validateOptions(params):
+##    config=readConfig()
+#    if params['rgb']!=None:
+#        params['rgb']=validateColors(params['rgb'])
+#        params['doPlot']=True
+#        rawConfiguration['doPlot']=True
+#    if params['labels']!=None:
+#        params['labels']=validateLabels(params['labels'])
+#        params['doPlot']=True
+#        rawConfiguration['doPlot']=True       
+#    if params['rgb']!=None and len(params['rgb'])<len(params['fileNames'])-1:
+#        fail(parser, 'too few colors specified to --RGB')
+#    if params['labels']!=None and len(params['labels'])<len(params['fileNames'])-1:
+#        fail(parser, 'too few labels specified to --labels')
+#    if params['chrom']==0: #Use filenames to guess the chromsome used
+#        params['chrom']=determineChromosome(params['fileNames']).replace('chr','')
+#        sys.stderr.write('WARNING! No chromosome was specified assuming: %s\n'%params['chrom'])
 
 
-#--- TODO:  Add support for reading plot options
-#    group = OptionGroup(parser, 'Plotting Options', 'Use these options to generate graphics and customize graphics.'
-#                    'It is believed that some of them bite.')
-#    group.add_option('-p', '--plot', dest='doPlot', action='store_true', default=False,
-#                      help='Generate graphical output')
-#    group.add_option('--RGB', type='string',  dest='rgb', action='callback', callback=optColorProcessor, 
-#                      help='Color specifications of different populations for graphing : ', metavar='colors')
-#    group.add_option('--labels', type='string', dest='labels', action='callback', callback=optLabelProcessor,
-#                      help='populations labels for graphing Example: ceu,yri,mkk', metavar='labels')
-#    parser.add_option_group(group)
-#    (options, args) = parser.parse_args()
-
-    
+   
 def getConfigOptions(configFile):
     
     config=readConfig(configFile)
@@ -50,22 +88,32 @@ def getConfigOptions(configFile):
     if config.has_option('parameters', 'chromosome'):
         configData['chrom']=config.getint('parameters', 'chromosome')
     else:
-        configData['chrom']=0
+        configData['chrom']=CHROM
     
     if config.has_option('parameters', 'window'):
         configData['win']=config.getint('parameters','window')
     else:
-        configData['win']=100
+        configData['win']=WIN
     
     if config.has_option('parameters','generations'):
         configData['nGens']=config.getfloat('parameters','generations')
     else:
-        configData['nGens']=6.0
+        configData['nGens']=N_GENS
         
     if config.has_option('parameters','saveFile'):
         configData['saveFile']=config.get('parameters','saveFile')
     else:
         configData['saveFile']='outSupportMix'
+        
+    if config.has_option('parameters','parallel'):
+        configData['nParallel']=config.get('parameters','parallel')
+    else:
+        configData['nParallel']=None
+    
+    if config.has_option('parameters','mapDir'):
+        configData['mapDir']=config.get('parameters','mapDir')
+    else:
+        configData['mapDir']=None
     
     #File related processing
     if config.has_section('data location'):
@@ -155,6 +203,12 @@ def writeConfigFile(configData,configFileName='outSupportMix.cfg'):
     config.set('parameters', 'window', configData['win'])
     config.set('parameters', 'generations', configData['nGens'])
     config.set('parameters', 'saveFile', configData['saveFile'])
+    config.set('parameters', 'saveFile', configData['saveFile'])
+    
+    if configData['nParallel']!=None:
+        config.set('parameters','parallel', configData['nParallel'])
+    if configData['mapDir']!=None:
+        config.set('parameters','mapDir', configData['mapDir'])
     
     if configData['correctFile']:
         baseDataDir, ancestryFile=os.path.split(configData['correctFile'])
@@ -259,6 +313,135 @@ def writeConfigFileNEW(configData,configFileName='outSupportMix.cfg'):
     with open(configFileName, 'wb') as configFile:
         configFile.write("#SupportMix configuration file.\n#To run type: SupportMix -C %s\n\n"%(configFileName))
         config.write(configFile)
+
+def validateColors(value):
+    conv=pylab.mpl.colors.ColorConverter()
+    try:
+        values=[conv.to_rgba(c) for c in value.split(',')]
+        return values
+    except:
+        supportConfig.fail(parser, 'color specification to %s incorrect' %"RGB")
+
+
+def validateLabels(value):
+    try: 
+        labels=value.split(',')
+        return labels
+    except:
+        supportConfig.fail(parser, 'label specification to %s incorrect' %"labels")
+
+
+def getParameters(rawConfiguration):
+    
+    rawConfiguration['chrom']=CHROM
+    rawConfiguration['win']=WIN
+    rawConfiguration['nGens']=N_GENS
+    rawConfiguration['saveFile']=SAVE_FILE
+    rawConfiguration['correctFile']=CORRECT_FILE
+    rawConfiguration['fileNames']=FILE_NAMES
+    rawConfiguration['doPlot']=DO_PLOT
+    rawConfiguration['labels']=RGB
+    rawConfiguration['rgb']=CONFIGFILE
+    rawConfiguration['configFile']=LABELS
+    rawConfiguration['isBeagle']=IS_BEAGLE
+    rawConfiguration['nParallel']=MAP_DIR
+    rawConfiguration['mapDir']=N_PARALLEL
+
+    #Command Line parsing of parameters
+    parser = OptionParser(usage=USAGE)
+    parser.add_option('-c', '--chromosome', type='str', dest='chrom',
+                      help='Chromsome being analyzed (default guessed from file names)', metavar='N')
+    parser.add_option('-w', '--window', type='int', dest='win',
+                      help='Number of SNPs in each window (default 100)', metavar='N')
+    parser.add_option('-g', '--generations', type='float', dest='nGens',
+                      help='Number of generations since admixture used in hmm. (default 6)', metavar='N')
+    parser.add_option('-s', '--save', type='str', dest='saveFile',
+                      help='Destination file to save output (default outSupportMix)', metavar='file')
+    parser.add_option('-a', '--ancestryFile', type='string', dest='correctFile',
+                      help='FILE contains correct classifications in tab delimited format with SNPs in rows and samples in columns (first two columns contain rsID and position. ', metavar='FILE')
+    parser.add_option('-C', '--config', type='string', dest='configFile',
+                      help='Use a config file instead of parameters', metavar='file')
+    parser.add_option('-P', type='int', dest='nParallel', metavar='N',
+                      help="Run up to N processes in parallel at the same time (default 1)")
+    parser.add_option('-m', '--map-dir', type='string', dest='mapDir', metavar='directory',
+                      help="path to a directory of genetic map files.  The directory must contain a series of files which names containing chrN where N varies over all chromosomes in the input files  (example: genetic_map_chr1_b36.txt and genetic_map_chr22_b36.txt).  These files contain three, white spaced dilimeted columns: position [bp], combined rate [cM/Mb], Genetic Map [cM].  Example files for human genome release 36 can be found at, ftp://ftp.hapmap.org/hapmap/recombination/2008-03_rel22_B36/rates/ and for relase 37 at, ftp://ftp.hapmap.org/hapmap/recombination/2011-01_phaseII_B37/genetic_map_HapMapII_GRCh37.tar.gz")
+    parser.add_option('--generateConfig', action='store_true', dest='generateConfig',
+                      help='Create a sample configuration file with default parameters')
+    parser.add_option('-t', action='store_true', dest='isBeagle',
+                      help='DEPRECATED Do not use!!!!!!!!!')
+    group = OptionGroup(parser, 'Plotting Options', 'Use these options to generate graphics and customize graphics.'
+                    'It is believed that some of them bite.')
+    group.add_option('-p', '--plot', dest='doPlot', action='store_true',
+                      help='Generate graphical output')
+    group.add_option('--RGB', type='string',  dest='rgb', metavar='colors',
+                      help='Color specifications of different populations for graphing : ')
+    group.add_option('--labels', type='string', dest='labels', metavar='labels',
+                      help='populations labels for graphing Example: ceu,yri,mkk')
+    parser.add_option_group(group)
+    
+    (cmdOptions, args) = parser.parse_args()
+    #Config file parsing
+    if cmdOptions.generateConfig:
+        sys.stdout.write("Generating default config file...\n")
+        defaultConfigFile="supportMixConfigExample.cfg"
+        rawConfiguration['correctFile']="ancestryfileHere.txt"
+        rawConfiguration['fileNames']=['sample1.txt','sample2.txt','myDataDirectoryHere/myAdmixedFileHere.txt']
+        writeConfigFile(rawConfiguration,defaultConfigFile)
+        sys.stdout.write("Configuration example saved to: %s\n"%defaultConfigFile)
+        sys.exit(0)
+        
+    if cmdOptions.configFile:
+        if DEBUG:
+            print "Running with options listed in:", os.path.abspath(cmdOptions.configFile)
+        if os.path.exists(cmdOptions.configFile):
+            configData=getConfigOptions(cmdOptions.configFile)
+            for k,value in configData.iteritems():
+                if value!=None:
+                    rawConfiguration[k]=value
+            #Added support for chromosome only and ancestry files on the config file
+            #         Is this really a stupid idea?  Should we ship a default config file instead?
+            #Francisco: Should probably be 86 now?
+#            if len(rawConfiguration['fileNames'])==1:  #no ancestral files given run in default with all HGDP populations
+#                admixed=rawConfiguration['fileNames']
+#                rawConfiguration['fileNames']=glob.glob(HGDP_BASENAME%rawConfiguration['chrom'])
+#                rawConfiguration['fileNames'].extend(admixed)
+#            elif len(rawConfiguration['fileNames'])==2:  #too few ancestral files given 
+#                fail(parser, 'Not enough ancestral populations were given')
+        else:
+            fail(parser, 'Invalid Config File Specified')
+    else:
+        cmdOptions.fileNames=args
+                    
+    #Overwrite the "raw" configuration values with command line values whenever
+    for key,value in cmdOptions.__dict__.iteritems():
+        if value!=None:
+            rawConfiguration[key]=value
+    #keep the original parameters
+    params=rawConfiguration.copy()
+    
+#    if len(params['fileNames'])<3:
+#        supportConfig.fail(parser, 'Not enough ancestral populations were given')
+    
+
+    if params['rgb']!=None:
+        params['rgb']=validateColors(params['rgb'])
+        params['doPlot']=True
+        rawConfiguration['doPlot']=True
+    if params['labels']!=None:
+        params['labels']=validateLabels(params['labels'])
+        params['doPlot']=True
+        rawConfiguration['doPlot']=True       
+    if params['rgb']!=None and len(params['rgb'])<len(params['fileNames'])-1:
+        fail(parser, 'too few colors specified to --RGB')
+    if params['labels']!=None and len(params['labels'])<len(params['fileNames'])-1:
+        fail(parser, 'too few labels specified to --labels')
+    if params['chrom']==0: #Use filenames to guess the chromsome used
+        params['chrom']=determineChromosome(params['fileNames']).replace('chr','')
+        warn('No chromosome was specified assuming: %s\n'%params['chrom'])
+        #sys.stderr.write('WARNING! No chromosome was specified assuming: %s\n'%params['chrom'])
+        
+    return params
+
 
 
 if __name__ =="__main__":
