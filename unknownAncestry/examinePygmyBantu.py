74244556c87261a05911cb786c4f3d3526c17c06
lgo2 <lgo2@208719b8-4595-0410-aefe-b1a4a67cc89b>
2011-06-07 08:17:36 +0000

    Changes to examinePygmyBantu to:     * Reread old results for quicker graphing     * Extra plots for paper     * commented code to remove individuals that are outliers in PC space.
    git-svn-id: https://forge.cornell.edu/svn/repos/admixture_mapping@82 208719b8-4595-0410-aefe-b1a4a67cc89b

--- unknownAncestry/examinePygmyBantu.py
+++ unknownAncestry/examinePygmyBantu.py
@@ -108,7 +108,8 @@ def saveTPedFile(fileName, winSize, chroms, subjects, allSNPNames, allSNPLocatio
     - `ddmixedClass`: list of classifications, one item per chromsome
     - `p`: list of posterior probability for every window
     """
-    nSubs=134#len(subjects)
+    nSubs=len(subjects)
+    print nSubs
     with open(fileName+'.tfam', 'w') as pedFile:
         for i, sub in enumerate(subjects[::2]):
             pedFile.write('%s %s 0 0 0 0\n' %(sub[:-2], sub[:-2]))
@@ -129,14 +130,62 @@ def saveTPedFile(fileName, winSize, chroms, subjects, allSNPNames, allSNPLocatio
     tPedFp.close()
     tPedProbFp.close()
 
+def readResults(saveFile):
+    # Reread data from previous run
+    import gzip, numpy as np
+    allAdmixedClass=[]
+    allAdmixedP=[]
+    allSNPLocations=[]
+    allSNPNames=[]
+
+    chroms=[]
+    snpNames=[]
+    snpLocations=[]
+    admixedClass=[]
+    admixedP=[]
+    i=-1; currChrom='1'
+    fpa=gzip.open(saveFile+'.tped.gz')
+    fpp=gzip.open(saveFile+'.Probs.tped.gz')
+    for l in fpa:
+        lp=fpp.readline()
+        i+=1
+        l=l.strip().split()
+        if l[0] != currChrom:
+            print currChrom, len(admixedClass), len(admixedP)
+            i=0
+            allSNPNames.append(np.asarray(snpNames))
+            allSNPLocations.append(np.asarray(snpLocations))
+            allAdmixedClass.append(np.asarray(admixedClass, np.short)-1)
+            allAdmixedP.append(np.asarray(admixedP, np.float))
+            chroms.append(currChrom)
+            snpNames=[]
+            snpLocations=[]
+            admixedClass=[]
+            admixedP=[]
+            currChrom=l[0]
+        snpNames.append(l[1])
+        snpLocations.append(int(l[3]))
+        if i%50!=0: continue
+        admixedClass.append(l[4:])
+        admixedP.append(lp.strip().split()[4:])
+
+    allSNPNames.append(np.asarray(snpNames))
+    allSNPLocations.append(np.asarray(snpLocations))
+    allAdmixedClass.append(np.asarray(admixedClass, np.short)-1) #Fix to suboutput windows only
+    allAdmixedP.append(np.asarray(admixedP, np.float))
+    chroms.append(currChrom)
+    fpa.close(); fpp.close()
+
+    return allAdmixedClass, allAdmixedP, allSNPLocations, allSNPNames
 
 ###############################################################################################
 ## Main
 ###############################################################################################
 chroms=range(1,23)
-winSize=50; nAncestral=72; nGens=20; winStep=50
+winSize=50; nAncestral=72; nGens=20; winStep=winSize
 fileNames=[LEMANDE, NGUMBA, TIKAR_S, BAKOLA, BAKA, BEDZAN]
 alphaFile='pygmy_bantu_alphas.txt'
+saveFile='pygmy_admixture_72b'
 # winSize=100; nAncestral=30; nGens=8; winStep=100
 # fileNames=['tmp/ancestral_ceu.chr%i.csv','tmp/admixed_ceu_yri.chr%i.csv']    
 # alphaFile='tmp/admixed_ceu_yri.alpha.chr22.csv'
@@ -145,6 +194,10 @@ alphaFile='pygmy_bantu_alphas.txt'
 classifier=pymvpa.LinearCSVMC(C=10)
 cvte=pymvpa.CrossValidation(classifier, pymvpa.NGroupPartitioner(3, attr='runtype'))
 alphas=[1-float(f.strip().split()[-1]) for f in open(alphaFile)]
+
+#Read results
+#allAdmixedClass, allAdmixedP, allSNPLocations, allSNPNames=readResults(saveFile)
+
 allAdmixedClass=[]
 allAdmixedP=[]
 allSNPLocations=[]
@@ -154,6 +207,17 @@ allSNPNames=[]
 for CHROM in chroms:
     subjects, labels, snpNames, snpLocations, vals=readFiles([f%CHROM for f in fileNames])
     origLabels=labels.copy()
+    # #crappy filter ################################
+    # idxT=np.array([ True,  True,  True,  True,  True,  True,  True,  True,  True, True,  True,  True,  True,  True,  True,  True,  True,  True,        True,  True, False, False,  True,  True,  True,  True,  True,        True,  True,  True,  True,  True,  True,  True,  True,  True,        True,  True,  True,  True,  True,  True,  True,  True,  True,        True,  True,  True,  True,  True,  True,  True,  True,  True,        True,  True,  True,  True,  True,  True,  True,  True,  True,        True,  True,  True,  True,  True,  True,  True,  True,  True,        True,  True,  True,  True,  True,  True,  True,  True,  True,        True,  True,  True,  True,  True,  True,  True,  True,  True,        True,  True,  True,  True,  True,  True,  True,  True,  True,        True,  True,  True,  True,  True,  True,  True,  True,  True,        True,  True,  True,  True,  True,  True,  True,  True,  True,        True,  True,  True,  True,  True,  True,  True,  True,  True,        True,  True,  True,  True,  True,  True,  True,  True,  True,        True,  True,  True,  True,  True,  True,  True,  True,  True,        True,  True,  True,  True,  True,  True,  True,  True,  True,        True,  True,  True,  True,  True,  True,  True,  True,  True,        True,  True,  True,  True,  True,  True,  True,  True,  True,        True,  True,  True,  True,  True,  True,  True,  True,  True,        True,  True,  True,  True,  True,  True,  True,  True,  True,        True,  True,  True,  True,  True,  True,  True,  True,  True,        True,  True,  True,  True,  True,  True,  True,  True, False,       False,  True,  True,  True,  True,  True,  True,  True,  True,        True,  True,  True,  True,  True,  True,  True,  True,  True,        True,  True,  True,  True,  True,  True,  True,  True,  True,        True,  True,  True,  True, False, False,  True,  True,  True,        True,  True,  True,  True,  True,  True,  True], dtype=bool)
+    # vals = vals[idxT, :]
+    # labels=labels[idxT]
+    # subs=['CALM007_A', 'CALM007_B', 'CAPB032_A', 'CAPB032_B', 'CAPM050_A', 'CAPM050_B']
+    # for i in range(len(subjects)):
+    #     for sub in subs:
+    #         subjects[i]=subjects[i][subjects[i]!=sub]
+    # print sum(map(len, subjects))
+    # #end crappy filter ######################################
+
     if len(fileNames)==6:  #Real data
         idxBantu=pylab.find((labels==0)+(labels==1)+(labels==2))
         idxPygmy=pylab.find((labels==3)+(labels==4)+(labels==5))
@@ -201,48 +265,47 @@ for CHROM in chroms:
     allSNPNames.append(snpNames)
     #Print summary of results
     print 'Chromosome %i: Percent Pygmy: %0.2g%% (%0.2g) ' %(CHROM,(admixedClass==1).sum()/float(np.prod(admixedClass.shape))*100, float(nAncestral)/len(idxPygmy)*100)
+#Save output in tped plink files
+saveTPedFile(saveFile, winSize, chroms, np.hstack(subjects[3:]), allSNPNames, allSNPLocations, allAdmixedClass,allAdmixedP)
 
 #Plot results of multiple Chromsomes
 pylab.figure(figsize=(19.5,5))
-nPygmy=admixedClass.shape[1]
+nPygmy=allAdmixedClass[0].shape[1]
 nWins=[x.shape[0] for x in allAdmixedClass]
-xStart=0.03
+xStart=0.03; totalWidth=.85; yStart=0.28; totalHeight=.63
 for i, CHROM in enumerate(chroms):
     admixedClass=allAdmixedClass[i]
     p=allAdmixedP[i]
     nWin=nWins[i]
     snpLocations=allSNPLocations[i]
-    xWidth=.84*(nWin/float(np.sum(nWins)))
-    pylab.axes([xStart, 0.29, xWidth, .52])
+    xWidth=totalWidth*(nWin/float(np.sum(nWins)))
+    pylab.axes([xStart, yStart, xWidth, totalHeight])
     pylab.imshow(((admixedClass*2-1)*p).T, interpolation='nearest', cmap=pylab.cm.RdBu)
-    pylab.axis('tight');     pylab.title('Chr %i' %CHROM, fontsize=8)
-    xtickPos=np.asarray([2, round(nWin/2), nWin-2], np.int)
+    pylab.axis('tight');     pylab.title('%i' %CHROM, fontsize=9)
+    xtickPos=np.asarray([3, round(nWin/2), nWin-3], np.int)
     pylab.axis('off'); 
     if xStart==0.03:
         pylab.axis('on'); pylab.xticks([]); 
-        pylab.yticks([0, 24,58,83,108,121,134], ['|', 'Bakola', '|','Baka','|','Bedzan', '|'], fontsize=8, rotation=90)
-    pylab.axes([xStart, 0.1, xWidth, .18])  #Average Pygmy Ancestry
+        pylab.yticks([0, 24,58,83,108,121,134], ['|', 'Bakola', '|','Baka','|','Bedzan', '|'], fontsize=9, rotation=90)
+    pylab.axes([xStart, 0.08, xWidth, .18])  #Average Pygmy Ancestry
     pylab.plot(admixedClass.mean(1))
     pylab.ylim([0.5, 1]); pylab.xlim(0, p.shape[0]); pylab.yticks([])
     pylab.xticks(xtickPos, np.asarray(np.round(snpLocations[::winStep][xtickPos]/1e6), np.int), fontsize=6)
     if xStart==0.03:
         pylab.yticks([.5,1], fontsize=8); pylab.ylabel('Percent Pygmy', fontsize=8);
-    if xStart>0.45 and xStart<0.5:
+    if xStart>0.47 and xStart<0.5:
         pylab.xlabel('Position [Mb]', fontsize=8)
     pylab.draw()
     xStart+=(xWidth+0.002)
-#Plot average Pygmy Ancestry per sample
-pylab.axes([0.92, 0.29, .07, .52]) 
-pylab.plot(np.vstack(allAdmixedClass).mean(0), range(nPygmy)); pylab.ylim(nPygmy, 0); pylab.yticks([]); pylab.title('Percent Pygmy'); pylab.xlim(0,1); pylab.xticks([0,.5,1], fontsize=8)
-pylab.plot(alphas, range(nPygmy)); pylab.ylim(nPygmy, 0); pylab.yticks([]); pylab.title('Percent Pygmy', fontsize=8); pylab.xlim(0,1); pylab.xticks([0,.5,1])
+# #Plot average Pygmy Ancestry per sample
+# pylab.axes([0.92, 0.29, .07, .52]) 
+# pylab.plot(np.vstack(allAdmixedClass).mean(0), range(nPygmy)); pylab.ylim(nPygmy, 0); pylab.yticks([]); pylab.title('Percent Pygmy'); pylab.xlim(0,1); pylab.xticks([0,.5,1], fontsize=8)
+# pylab.plot(alphas, range(nPygmy)); pylab.ylim(nPygmy, 0); pylab.yticks([]); pylab.title('Percent Pygmy', fontsize=8); pylab.xlim(0,1); pylab.xticks([0,.5,1])
 #Colorbar
-pylab.axes([0.925, 0.1, .06, .18]);  pylab.xlim(-1, 1); pylab.ylim(-1, 1); pylab.axis('off')
-pylab.colorbar(fraction=.5, aspect=4, orientation='horizontal', ticks=[-.99, 0, .99])
-pylab.text(-1,-1.5, 'Bantu', fontsize=8)
-pylab.text(1,-1.5, 'Pygmy', fontsize=8, horizontalalignment='right')
-
-#Save output in tped plink files
-saveTPedFile('pygmy_admixture_72b', winSize, chroms, np.hstack(subjects[3:]), allSNPNames, allSNPLocations, allAdmixedClass,allAdmixedP)
+pylab.axes([0.88, .08, .06, .9]);  pylab.xlim(-1, 1); pylab.ylim(-1, 1); pylab.axis('off')
+cbar=pylab.colorbar(fraction=.1, ticks=[-1, -0.5,  0, 0.5, 1])
+cbar.ax.set_yticklabels(['1.0 Bantu', '0.5', '0', '0.5', '1.0 Pygmy'], fontsize=6)
+pylab.suptitle('Chromosomes')
 
 
 # If simulated data calculate Success Rate
@@ -263,3 +326,91 @@ if locals().get('correctFile'):
     pylab.xlabel('Position along %s' %CHROM);  pylab.axis('tight'); pylab.title('True ancestry')
 
 pylab.show()
+
+#############################################################
+# Addendum code
+#############################################################
+
+#########################################
+#compare to structure
+pylab.figure()
+pylab.plot(np.vstack(allAdmixedClass).mean(0)); 
+pylab.plot(alphas); 
+pylab.xlim(134, 0); 
+pylab.xticks([0, 24,58,83,108,121,134], ['|', 'Bakola', '|','Baka','|','Bedzan', '|'], fontsize=10)
+pylab.title('Percent Pygmy'); 
+pylab.ylim(0,1.01); pylab.yticks([0,.5,1], fontsize=10); 
+pylab.legend(['SupportMix', 'STRUCTURE'], 4)
+
+###########################
+# Count ancestry track length and plot
+pylab.figure()
+ones=[]
+twos=[]
+for chrom in chroms:#[2]:
+    chrom-=min(chroms)
+    admixedClass=allAdmixedClass[chrom]
+    snpPos=allSNPLocations[chrom][::winSize]
+    state=admixedClass[0,:]
+    starts=np.tile(int(snpPos[0]), 134)
+    ends=int(snpPos[0])
+    for i in range(admixedClass.shape[0]): # range(396, 500):
+        changed=state!=admixedClass[i,:]
+        if np.any(changed):
+            ones.extend(ends-starts[np.logical_and(state==0,changed)])
+            twos.extend(ends-starts[np.logical_and(state==1,changed)])
+            #divide above into ones and zeros
+            starts[changed]=int(snpPos[i])
+            state=admixedClass[i,:]
+        ends=int(snpPos[i])
+
+pylab.hist(ones, np.logspace(4,8, 80), histtype='step', normed=True); pylab.xscale('log')
+pylab.hist(twos, np.logspace(4,8, 80), histtype='step', normed=True); pylab.xscale('log')
+pylab.legend(['Bantu', 'Pygmy'])
+pylab.xlabel('Length [bp]')
+pylab.xlabel('Length [bp]')
+pylab.ylabel('Normalized number of segmensts')
+
+adsf
+
+#########################################
+#Plot specific chromome with ancestry averages
+pylab.figure()
+n=2
+
+admixedClass=allAdmixedClass[n]
+p=allAdmixedP[n]
+nWin=nWins[n]
+chrom=chroms[n]
+snpLocations =allSNPLocations[n]
+pylab.imshow(((admixedClass*2-1)*p).T, interpolation='nearest', cmap=pylab.cm.RdBu)
+pylab.axis('tight');     pylab.title('Chr %i' %chrom, fontsize=8)
+xtickPos=np.linspace(2, nWin-2, 20).astype(np.int)
+pylab.yticks([0, 24,58,83,108,121,134], ['|', 'Bakola', '|','Baka','|','Bedzan', '|'], fontsize=8, rotation=90)
+pylab.xticks(xtickPos, np.asarray(np.round(snpLocations[::winStep][xtickPos]/1e6), np.int), fontsize=10)
+pylab.xlabel('Position [Mb]')
+nTransitions=np.abs(np.diff(admixedClass, axis=0)).sum(1)
+#pylab.plot(134-nTransitions/float(np.max(nTransitions))*134, 'w', linewidth=.75)
+percentPygmy=134-np.sum(admixedClass==1, 1)
+#pylab.plot(percentPygmy, color=[0,1,0])
+pylab.xlim(100,750)
+cbar=pylab.colorbar(fraction=.1, ticks=[-1, -0.5,  0, 0.5, 1])
+cbar.ax.set_yticklabels(['1.0 Bantu', '0.5', '0', '0.5', '1.0 Pygmy'])
+
+
+###########################
+# EMMAX manhattanplot
+p=np.asarray([l.split()[2] for l in open('EMMAX_2011_05_09/emmax_ancestry_BN.ps')], np.float)
+chrom=np.asarray([l.split()[0] for l in open('EMMAX_2011_05_09/pygmy_admixture_72b.short.tped')], np.int)
+pos=np.asarray([l.split()[3] for l in open('EMMAX_2011_05_09/pygmy_admixture_72b.short.tped')], np.int)
+start=0; color='b'
+for i in range(1,23):
+    idx=(chrom==i)
+    pylab.scatter(start+pos[idx], -np.log10(p[idx]), s=4, c=color, linewidths=0)
+    start+=np.max(pos[idx])
+    color = 'r' if color=='b' else 'b'
+
+pylab.axis([-1e7, 2.89e9, 0, 5])
+pylab.xticks([])
+pylab.xlabel('Position along Chromsomes')
+pylab.ylabel('-log10(p)')
