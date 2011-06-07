471d0dc4383e1421eac1d1a33c11eb9ea1d423bb
lgo2 <lgo2@208719b8-4595-0410-aefe-b1a4a67cc89b>
2011-06-07 08:17:51 +0000

    Changes to make code work over multiple chromosomes   * Works on tped, tfam files   * Handles error when: No SNPs are found at specified chromsome; mapfile errors.   * Warns when genetic map "seems" wrong   * greatly expands accuracy estimates   * saveFiles now outputs:       * short form of tped files       * corrected tfam files       * file with start and end positions of windows       * file with legends for populations
    git-svn-id: https://forge.cornell.edu/svn/repos/admixture_mapping@85 208719b8-4595-0410-aefe-b1a4a67cc89b

--- runCommand.sh
+++ runCommand.sh
@@ -1,14 +1,15 @@
 #./build/exe.linux-x86_64-2.6/SupportMix -w 400 ceu_yri/ancestral_ceu.chr22.csv ceu_yri/ancestral_yri.chr22.csv /home/local/CB/fja32/workspace/SupportMix/src/ceu_yri/admixed_ceu_yri.chr22.csv
 
-#using TPED file for admixed
-# SupportMix -w100 -c22 -a data/tests/admixed_origin_ceu_yri.chr22.tped.gz \
-#     data/tests/ancestral_ceu.tped.gz \
-#     data/tests/ancestral_yri.tped.gz \
-#     data/tests/admixed_ceu_yri.tped.gz
+SupportMix -w100 -c22 -m data/hapmap2 -a data/tests/admixed_origin_ceu_yri.chr22.tped.gz \
+    data/tests/ancestral_ceu.tped.gz \
+    data/tests/ancestral_yri.tped.gz \
+    data/tests/admixed_ceu_yri.tped.gz
+
+echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
 
 SupportMix -w100 -t -a data/tests/admixed_origin_ceu_yri.chr22.tped.gz \
+    -m data/hapmap2/ \
     data/tests/ancestral_ceu.chr22.csv.gz \
     data/tests/ancestral_yri.chr22.csv.gz \
     data/tests/admixed_ceu_yri.chr22.csv.gz
-
 
