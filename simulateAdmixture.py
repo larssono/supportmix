93389ce7b00148f86873cacdba4b86cf695a1b51
lgo2 <lgo2@208719b8-4595-0410-aefe-b1a4a67cc89b>
2011-06-07 08:17:31 +0000

    Changes simulateAdmixture to act as standalone command. Adds:     * Support for command line arguments.     * Support for n-way admixture     * outputs tped files instead of hapmap phased files.     * changes ordering of mate pairs to preserve order of haploid genomes.
    git-svn-id: https://forge.cornell.edu/svn/repos/admixture_mapping@81 208719b8-4595-0410-aefe-b1a4a67cc89b

--- simulateAdmixture.py
+++ simulateAdmixture.py
@@ -1,41 +1,69 @@
-import sys, numpy as np, scipy.stats as stats
-sys.path.append('../')
+import sys
+import numpy as np
+import scipy.stats as stats
+from optparse import OptionParser, OptionValueError
+
 import fileReader
+import supportConfig
+import popgen
+
+USAGE="""%prog [options] ancestralFile1 ancestralFile2 [...]
+
+where the ancestralFiles contain phased samples in tab delimited
+format snps in rows and samples in columns.  One row of column headers
+and two columns of header information (i.e. rsId and position).  The
+files have to be from the same chromosome indicated by including
+chr[0-9]* in the name.
+"""
+F_GM=supportConfig.createDataPath('data/hapmap2/genetic_map_chr%s_b36.txt')
+
 
 def poissonMating(pop1, pop2, snpPos, mapFile,  nGens=1, percentPop1=0.2):
-    """Caries out mating in similar manner to Hapmix """
-    nSNPs, nOffspring=pop1.shape
-    outPop=np.empty_like(pop1)
-    outPopOrig=np.empty(pop1.shape, dtype=np.byte)
-    #Read map and calculate recombination distances
-    map=np.array([l.strip().split() for l in open(mapFile).readlines()[1:]], np.float)
-    dM=np.diff(map[:,2])/100*nGens  #morgans x  generations
+    """Depricated use poissonMultiMate instead."""
+    return poissonMultiMating([pop1, pop2], snpPos, mapFile, nGens, percentPop1)[0:2]
+
+def poissonMultiMating(pops, snpPos, mapFile, nGens, percentPop1=0.5):
+    """Performs mating between individuals in pops using a Poisson model for the recombination events.
     
+    Arguments:
+    - `pops`: List of two numpy array each with the same nSNPs x nOffspring dimensions
+    - `snpPos`: List of positions of all nSNPs basepairs in bp position
+    - `mapFile`: File with translations from bp positions to genetic map positions
+    - `nGens`: Number of generations to simulate (defualt=1)
+    - `percentPop1`: Percentage of alleles from first population in pops (default=.5)
+    """
+    #Set up output variables
+    nPops=len(pops)
+    nSNPs, nOffspring=pops[0].shape
+    outPop=np.empty_like(pops[0])
+    outPopOrig=np.empty(pops[0].shape, dtype=np.byte)
+    #Read map and calculate recombination distances
+    gm=popgen.geneticMap(mapFile)
+    gmPos=gm.pos2gm(snpPos)
+    dM=np.diff(gmPos)/100.*nGens #morgans x generations
+    #Determine probabilites of differen ancestral populations
+    alphas=np.ones(nPops)*(1-percentPop1)/(nPops-1)
+    alphas[0]=percentPop1
+    print alphas, 
+    #Step through each offspring and assign haploid genomes
     for i in range(nOffspring):
-        alpha=percentPop1
-        #alpha=stats.beta.rvs(BETA_BETA,BETA_ALPHA)  #Determine percentage of POP1 
-        recombPos=map[dM>np.random.uniform(size=len(map)-1), 0] #Determine bp positions of recomb.
+        recombPos=gmPos[dM>np.random.uniform(size=len(gmPos)-1)] #Determine bp positions of recomb.
         j=0
         for pos in recombPos:  #Step through locations where switch happens
-            origin=int(np.random.uniform()>alpha)  #0 = pop1, 1=pop2
-            while snpPos[j]<pos:  
-                if origin==0:
-                    outPop[j, i] = pop1[j,i]
-                    outPopOrig[j,i]=0
-                else:
-                    outPop[j, i] = pop2[j,i]
-                    outPopOrig[j,i]=1
+            origin=np.nonzero(np.random.uniform()<=np.cumsum(alphas))[0].min() #0=pop1, 1=pop2 ...
+            while gmPos[j]<pos: 
+                outPop[j, i] = pops[origin][j,i]
+                outPopOrig[j,i]=origin
                 j+=1
-        origin=int(np.random.uniform()>alpha)  #0 = pop1, 1=pop2
+        origin=int(np.nonzero(np.random.uniform()<=np.cumsum(alphas))[0].min()) #0=pop1, 1=pop2 ...
         while j<nSNPs:  #Fill in end of haplotype
-            if origin==0:
-                outPop[j, i] = pop1[j,i]
-                outPopOrig[j,i]=0
-            else:
-                outPop[j, i] = pop2[j,i]
-                outPopOrig[j,i]=1
+            outPop[j, i] = pops[origin][j,i]
+            outPopOrig[j,i]=origin
             j+=1
-    return outPop, outPopOrig
+    np.set_printoptions(precision=2)
+    print  ' -->', np.asarray([np.sum(outPopOrig==i)/float(outPopOrig.size) for i in range(nPops)]), '%  in', np.sum(np.diff(outPopOrig, axis=0)!=0, axis=0)+1, 'blocks across individuals'
+    return outPop, outPopOrig, gmPos
+
 
 def saveHaplotypes(filename, subjectNames, snpNames, snpPos, snpVals):
     """Saves file in same format at HapMap3 phased data"""
@@ -48,12 +76,41 @@ def saveHaplotypes(filename, subjectNames, snpNames, snpPos, snpVals):
         fp.write('\n')
     fp.close()
 
-def readFiles(files):
+def saveTped(filename, subjectNames, snpNames, snpPos, mapPos, snpVals, chrom):
+    """Saves file in tped and tfam format"""
+    nSubs=len(subjectNames)
+    with open(filename+'.tfam', 'w') as tFamFp:
+        pedPadding="0 %s 0 0 0 0 0"
+        for subject in subjectNames[::2]:
+            tFamFp.write("%s\n"%pedPadding%subject.replace('_a', ''))
+    with open(filename+'.tped', 'w') as fp:
+        for i, (rsId, rsPos, rsMap) in enumerate(zip(snpNames, snpPos, gmPos)):
+            fp.write('%s %s %0.5g %i' %(chrom, rsId, rsMap, rsPos))
+            fp.write(' %s'*nSubs %tuple(snpVals[i, :]))
+            fp.write('\n')
+
+def readFiles(files, fileType='beagle', chrom=None):
     nFiles=len(files)
-    files=fileReader.concurrentFileReader(*files)
-    subjects=files.next()[0]
+    if fileType=='beagle':
+        files=fileReader.concurrentFileReader(*files)
+        subjects=files.next()[0]
+    elif fileType=='tped':
+        tfams=[f.replace('.tped', '.tfam') for f in files]
+        tfams=[fileReader.openfile(f) for f in tfams]
+        subjects=[]
+        for f in tfams:
+            subs=[[l.split(None, 1)[0]+'_a',l.split(None, 1)[0]+'_b']  for l in f]
+            subjects.append(np.asarray(sum(subs, [])))
+        files=fileReader.concurrentFileReader(*files, nHeaders=0, key=[0,1], nLabels=4)
+    else:
+        sys.stderr.write('ERROR: Filetype has to be either beagle or tped')
+        sys.exit()
     snpNames=[]; snpPos=[];  pops=[[] for i in range(nFiles)]
     for s, l in files:
+        if fileType=='tped':
+            if chrom!=None and chrom!=s[0]:
+                continue
+            s=[s[1], s[3]]
         snpNames.append(s[0])
         snpPos.append(int(s[1]))
         for i in range(nFiles):
@@ -63,30 +120,73 @@ def readFiles(files):
     nPops=[l.shape[1] for l in pops]
     return pops,  nPops, subjects, nSNPs, snpPos, snpNames
 
-if __name__ == '__main__':
-    CHR='chr22'
-    DATA_DIR='../../human_genome_data/'
-    POP_FILES=[DATA_DIR+'hapmap3/CEU/TRIOS/hapmap3_r2_b36_fwd.consensus.qc.poly.%s_ceu.phased.gz'%CHR,
-               DATA_DIR+'hapmap3/YRI/TRIOS/hapmap3_r2_b36_fwd.consensus.qc.poly.%s_yri.phased.gz'%CHR]
-    F_GM=DATA_DIR+'/hapmap2/genetic_map_%s_b36.txt'%CHR
-    BETA_ALPHA=12
-    BETA_BETA=3
-    NOFFSPRING=6
-    NGENS=8
 
-    pops, nPops, subjects, nSNPs, snpPos, snpNames = readFiles(POP_FILES)
-    pop1, pop2=pops
-    nPop1, nPop2=nPops
-    idxPop1=np.random.permutation(nPop1) #Pick mating pairs
-    idxPop2=np.random.permutation(nPop2)
-    admixedPop, admixedOrigin=poissonMating(pop1[:,idxPop1[:NOFFSPRING]],
-                                            pop2[:,idxPop2[:NOFFSPRING]],
-                                            snpPos, F_GM, NGENS)
+def permuteIdx(n, nOffspring):
+    """Permutes the order of indexes 
+    
+    Arguments:
+    - `n`: number of samples to be permuted
+    """
+    idx=2*np.random.permutation(n/2)  #Permutes every other sample
+    idx=np.vstack((idx, idx+1)).flatten(1) #adds matching haploid genome
+    return np.hstack((np.random.permutation(idx[:nOffspring]), idx[nOffspring:])) #re-permutes the mate individuals
+    
+
+
+def optSaveFileProcessor(option, opt_str, value, parser):
+    try: 
+        parser.values.saveFiles=value.split(',')
+    except:
+        raise OptionValueError('label specification to %s incorrect' %opt_str)
+
+
+if __name__ == '__main__':
+    parser = OptionParser(usage=USAGE)
+    parser.add_option('-c', '--chromosome', type='string', dest='chrom', default='22',
+                      help='Chromsome being analysed (defualt 22)', metavar='N')
+    parser.add_option('-s', '--save', type='string', dest='saveFiles', action='callback', 
+                      callback=optSaveFileProcessor, metavar='files',
+                      help='prefixes of outfiles (example ceu_chr22,yri_chr22,admixed_ceu_yri_chr22')
+    parser.add_option('-n', type='int', dest='nOffspring', default=6,
+                      help='number of offspring to create (defualt 6)', metavar='N')
+    parser.add_option('-g', '--nGens', type='int', dest='nGens', default=8,
+                      help='number of generations since start of admixture', metavar='N')
+    parser.add_option('-a', '--alpha', type='float', dest='alpha', default=.5,
+                      help='Ratio of populations', metavar='N')
+    parser.add_option('-f', '--fileType', type='str', dest='fileType', default='beagle',
+                      help='Input Filetype, can be either phase or tped')
+    parser.add_option('--geneticMap', type='str', dest='geneticMapFile', default=None,
+                      help='genetic map for specified chromosome')
+    (options, args) = parser.parse_args()
+
+    fileNames=args
+    if options.saveFiles==None:
+        options.saveFiles=['anc%i_chr%s'%(i+1, options.chrom) for i in range(len(fileNames))]
+        options.saveFiles.append('admixed_chr%s'%options.chrom)
+    if options.fileType not in ['beagle', 'tped']:
+        sys.stderr.write('Input file has to be in plink tped or simplified haplotype format')
+        sys.exit(1)
+    if options.geneticMapFile==None: #and options.fileType=='beagle':
+        options.geneticMapFile=F_GM%options.chrom
+
+    pops, nPops, subjects, nSNPs, snpPos, snpNames = readFiles(fileNames,
+                                                               fileType=options.fileType,
+                                                               chrom=options.chrom)
+    #determine which will mate with each other.
+    indexes=[permuteIdx(n, options.nOffspring) for n in nPops]
+
+    matePairs=[pops[i][:, index[:options.nOffspring]] for i, index in enumerate(indexes)]
+    mateSubjects=[subjects[i][index[:options.nOffspring]] for i, index in enumerate(indexes)]
+    ancestralPops=[pops[i][:, index[options.nOffspring:]] for i, index in enumerate(indexes)]
+    ancestralSubjects=[subjects[i][index[options.nOffspring:]] for i, index in enumerate(indexes)]
+    #Running the mating
+    admixedPop, admixedOrigin, gmPos=poissonMultiMating(matePairs, snpPos, options.geneticMapFile,
+                                                        options.nGens, percentPop1=options.alpha)
+
+
     #Save populations
-    saveHaplotypes('ancestral_ceu.%s.csv'%CHR, subjects[0][idxPop1[NOFFSPRING:]], snpNames, 
-                   snpPos, pop1[:,idxPop1[NOFFSPRING:]])
-    saveHaplotypes('ancestral_yri.%s.csv'%CHR, subjects[1][idxPop2[NOFFSPRING:]], snpNames, 
-                   snpPos,pop2[:,idxPop2[NOFFSPRING:]])
-    saveHaplotypes('admixed_ceu_yri.%s.csv'%CHR, ['ind']*NOFFSPRING, snpNames, snpPos, admixedPop)
-    saveHaplotypes('admixed_ceu_yri_origin.%s.csv'%CHR, ['ind']*NOFFSPRING, snpNames, snpPos, admixedOrigin)
+    for i in range(len(nPops)):
+        saveTped(options.saveFiles[i], ancestralSubjects[i], snpNames, snpPos, gmPos, ancestralPops[i], options.chrom)
+    saveTped(options.saveFiles[-1], ['ind']*options.nOffspring, snpNames, snpPos, gmPos, admixedPop, options.chrom)
+    saveTped(options.saveFiles[-1]+'_origin', ['ind']*options.nOffspring, snpNames, snpPos, gmPos, admixedOrigin, options.chrom)
 
